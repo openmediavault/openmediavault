85954426fe79090d240155f75dff141af43a53b7
Volker Theile <votdev@gmx.de>
2014-03-05 18:07:35 +0000

    * Improve code * Update locales

--- deb/openmediavault/var/www/openmediavault/js/omv/window/MessageBox.js
+++ deb/openmediavault/var/www/openmediavault/js/omv/window/MessageBox.js
@@ -165,7 +165,7 @@ Ext.define("OMV.window.MessageBox", {
 					if(!Ext.isEmpty(error.trace))
 						text += error.trace;
 					details.update(OMV.util.Format.whitespace(
-					  Ext.util.Format.htmlEncode(text), "pre"));
+					  Ext.util.Format.htmlDecode(text), "pre"));
 					if(details.isVisible()) {
 						details.hide();
 						c.setText(_("Show details"));
@@ -200,7 +200,7 @@ Ext.define("OMV.window.MessageBox", {
 						cls: me.baseCls + "-text",
 						value: (Ext.isEmpty(error.message) ?
 						  _("An error has occured") :
-						  Ext.util.Format.htmlEncode(error.message))
+						  Ext.util.Format.htmlDecode(error.message))
 					}]
 				}]
 			}, details = Ext.create("Ext.panel.Panel", {
