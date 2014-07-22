7cf02c0023bc7c715e197b64cfd161349019281d
Volker Theile <votdev@gmx.de>
2014-07-22 09:34:18 +0000

    Improve code.

--- deb/openmediavault/var/www/openmediavault/js/omv/workspace/window/plugin/ConfigObject.js
+++ deb/openmediavault/var/www/openmediavault/js/omv/workspace/window/plugin/ConfigObject.js
@@ -23,17 +23,23 @@
  * @ingroup webgui
  * @class OMV.workspace.window.plugin.ConfigObject
  * @derived Ext.AbstractPlugin
- * A plugin that adds support to the RPC mechanism to load data
- * by a given UUID. The UUID must be a property of the parent/owner
- * class.
+ * A plugin that adds support to the RPC mechanism to load data by a given
+ * id. The id must be a property of the parent/owner class.
+ * @param idProperty The name of the field treated as this configuration
+ *   object's unique id. Defaults to 'uuid'.
+ * @param newIdValue The value of the id that identifies a new configuration
+ *   object. Defaults to OMV.UUID_UNDEFINED.
  */
 Ext.define("OMV.workspace.window.plugin.ConfigObject", {
 	extend: "Ext.AbstractPlugin",
 	alias: [ "plugin.configobject", "plugin.dbobject" ],
 
+	idProperty: "uuid",
+	newIdValue: OMV.UUID_UNDEFINED,
+
 	/**
 	 * The class constructor.
-	 * The UUID of the database/configuration object MUST be part of
+	 * The id of the database/configuration object MUST be part of
 	 * the client component to which this plugin is associated to.
 	 */
 	constructor: function() {
@@ -41,31 +47,37 @@ Ext.define("OMV.workspace.window.plugin.ConfigObject", {
 		me.callParent(arguments);
 		var client = me.getCmp();
 		// The client must have an 'uuid' property.
-		if(Ext.isDefined(client.uuid)) {
+		if (Ext.isDefined(client[me.idProperty])) {
+			// Apply the 'isNew' function which is used to check whether the
+			// configuration object already exists or is a new one.
 			Ext.apply(client, {
 				isNew: function() {
-					return (client.uuid == OMV.UUID_UNDEFINED);
-				},
-				autoLoadData: !((client.mode == "local") ||
-				  (client.uuid == OMV.UUID_UNDEFINED)),
-				closeIfNotDirty: !(client.uuid == OMV.UUID_UNDEFINED),
+					return (client[me.idProperty] == me.newIdValue);
+				}
+			});
+			// Set other fields.
+			Ext.apply(client, {
+				autoLoadData: !((client.mode == "local") || client.isNew()),
+				closeIfNotDirty: !client.isNew(),
 				getRpcGetParams: me.interceptAfter(client, "getRpcGetParams"),
 				getRpcSetParams: me.interceptAfter(client, "getRpcSetParams")
 			});
 		} else {
 			Ext.Error.raise(Ext.String.format("Failed to initialize " +
-			  "plugin. The property 'uuid' is missing in class '{0}'.",
-			  Ext.getClassName(client)));
+			  "plugin. The property '{0}' is missing in class '{1}'.",
+			  me.idProperty, Ext.getClassName(client)));
 		}
 	},
 
 	interceptAfter: function(object, methodName) {
+		var me = this;
 		var method = object[methodName] || Ext.emptyFn;
 		return (object[methodName] = function() {
+			var o = {};
+			o[me.idProperty] = object[me.idProperty];
 			var params = method.apply(this, arguments);
-			return Ext.apply(params || {}, {
-				uuid: object.uuid
-			});
+			params = Ext.apply(params || {}, o);
+			return params;
 		});
 	}
 });
