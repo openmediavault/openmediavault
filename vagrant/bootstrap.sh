dd2fd186431dd1a40a2d658b567b58e6046a0a62
votdev <votdev@gmx.de>
2016-12-06 22:18:33 +0100

    Add Vagrant build scripts.

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#
+# This file is part of openmediavault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2016 Volker Theile
+#
+# openmediavault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# openmediavault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with openmediavault. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+# Append user 'vagrant' to group 'ssh', otherwise the user is not allowed
+# to log in via SSH.
+usermod --groups ssh --append vagrant
+
+# Install openmediavault.
+cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb http://packages.openmediavault.org/public erasmus main
+# deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus main
+## Uncomment the following line to add software from the proposed repository.
+# deb http://packages.openmediavault.org/public erasmus-proposed main
+# deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus-proposed main
+## This software is not part of OpenMediaVault, but is offered by third-party
+## developers as a service to OpenMediaVault users.
+# deb http://packages.openmediavault.org/public erasmus partner
+# deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus partner
+EOF
+
+export LANG=C
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+apt-get update
+apt-get --allow-unauthenticated install openmediavault-keyring
+apt-get --yes --force-yes --auto-remove --show-upgraded \
+	--no-install-recommends \
+	--option Dpkg::Options::="--force-confdef" \
+	--option DPkg::Options::="--force-confold" \
+	install postfix openmediavault
+
+# Initialize the system and database.
+omv-initsystem
+
+# Rebuild configurations.
+omv-mkconf interfaces
+omv-mkconf issue
+
+# Display the login information.
+cat /etc/issue
