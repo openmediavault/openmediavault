ef67fee03032aa4253a03e6c6acf48a8b1ad5ed5
votdev <votdev@gmx.de>
2017-01-16 22:54:16 +0100

    Adapt plugin to omv-confdbadm command.

--- deb/openmediavault-ldap/usr/share/openmediavault/confdb/delete.d/openmediavault-ldap.sh
+++ deb/openmediavault-ldap/usr/share/openmediavault/confdb/delete.d/openmediavault-ldap.sh
@@ -21,23 +21,10 @@
 
 set -e
 
-. /etc/default/openmediavault
 . /usr/share/openmediavault/scripts/helper-functions
 
-case "$1" in
-	purge)
-		# Remove the configuration data.
-		echo "Updating configuration database ..."
-		omv-confdbadm delete "${DPKG_MAINTSCRIPT_PACKAGE}"
-	;;
-
-	remove|upgrade|failed-upgrade|abort-install|abort-upgrade|disappear)
-	;;
-
-	*)
-		echo "postrm called with unknown argument '$1'" >&2
-		exit 1
-	;;
-esac
+if omv_config_exists "/config/services/ldap"; then
+	omv_config_delete "/config/services/ldap"
+fi
 
 exit 0
