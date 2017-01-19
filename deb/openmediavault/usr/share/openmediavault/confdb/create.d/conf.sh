be66a9695fc4e7323f33625157954fd2e2bc8349
votdev <votdev@gmx.de>
2017-01-19 23:00:44 +0100

    Relocate configuration database creation.

---
+++ deb/openmediavault/usr/share/openmediavault/confdb/create.d/conf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2017 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /etc/default/openmediavault
+. /usr/share/openmediavault/scripts/helper-functions
+
+# Install the configuration database.
+if [ ! -e "${OMV_CONFIG_FILE}" ]; then
+	cp ${OMV_CONFIG_TEMPLATE_FILE} ${OMV_CONFIG_FILE}
+	chmod 660 ${OMV_CONFIG_FILE}
+	chown :openmediavault-config ${OMV_CONFIG_FILE}
+fi
+
+exit 0
