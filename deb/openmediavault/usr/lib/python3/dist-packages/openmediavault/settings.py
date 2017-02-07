905628294b34911b686b04f12a52e0a48dbb77f1
votdev <votdev@gmx.de>
2017-02-07 18:52:53 +0100

    Continue to implement parts of the OMV framework in Python.

--- deb/openmediavault/usr/lib/python3/dist-packages/openmediavault/settings.py
+++ deb/openmediavault/usr/lib/python3/dist-packages/openmediavault/settings.py
@@ -25,57 +25,65 @@ import openmediavault
 
 DEFAULT_FILE = "/etc/default/openmediavault"
 
-# Import global variables from '/etc/default/openmediavault'.
-__etc_default_dict = {}
-with open(DEFAULT_FILE) as reader:
-	for line in reader.readlines():
-		m = re.match(r"^(OMV_([A-Z0-9_]+))=(\")?([^\"]+)(\")?$",
-			line.strip())
-		if not m:
-			continue
-		# Append variable, e.g. SCRIPTS_DIR
-		__etc_default_dict[m.group(2)] = m.group(4)
-		# !!! DEPRECATED !!!
-		# Append variable, e.g. OMV_SCRIPTS_DIR (equal to PHP OMV framework)
-		__etc_default_dict[m.group(1)] = m.group(4)
-locals().update(__etc_default_dict)
-
 class Environment(object):
+	_values = {}
+
 	"""
 	Access the environment variables defined within the file
 	/etc/default/openmediavault.
 	"""
 
+	@staticmethod
+	def load():
+		with open(DEFAULT_FILE) as reader:
+			for line in reader.readlines():
+				m = re.match(r"^(OMV_([A-Z0-9_]+))=(\")?([^\"]+)(\")?$",
+					line.strip())
+				if not m:
+					continue
+				# Append variable, e.g. SCRIPTS_DIR
+				Environment._values[m.group(2)] = m.group(4)
+				# !!! DEPRECATED !!!
+				# Append variable, e.g. OMV_SCRIPTS_DIR (equal to PHP OMV framework)
+				Environment._values[m.group(1)] = m.group(4)
+
+	@staticmethod
 	def get(key, default=None):
-		globals_dict = globals()
-		if key in globals_dict:
-			return globals_dict[key]
+		if key in Environment._values:
+			return Environment._values[key]
 		if default is None:
 			raise KeyError(
 				"The environment variable '%s' does not exist in '%s'" %
 				(key, DEFAULT_FILE))
 		return default
 
+	@staticmethod
 	def get_str(key, default=None):
 		value = __class__.get(key, default)
 		return str(value)
 
+	@staticmethod
 	def get_bool(key, default=None):
 		value = __class__.get(key, default)
 		return openmediavault.bool(value)
 
+	@staticmethod
 	def get_int(key, default=None):
 		value = __class__.get(key, default)
 		return int(value)
 
+	@staticmethod
 	def get_float(key, default=None):
 		value = __class__.get(key, default)
 		return float(value)
 
+	@staticmethod
 	def set(key, value):
-		globals_dict = globals()
 		result = None
-		if key in globals_dict:
-			result = globals_dict[key]
-		globals_dict[key] = value
+		if key in Environment._values:
+			result = Environment._values[key]
+		Environment._values[key] = value
 		return result
+
+# Auto-import environment variables from '/etc/default/openmediavault'.
+Environment.load()
