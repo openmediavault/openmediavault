3d3cce8c1057cc9542b3ea8102e9bd17622fc8b3
votdev <votdev@gmx.de>
2017-02-24 09:57:47 +0100

    Update package repository for OMV4.

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -27,15 +27,15 @@ usermod --groups ssh --append vagrant
 
 # Install openmediavault.
 cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
-deb http://packages.openmediavault.org/public erasmus main
-# deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus main
+deb http://packages.openmediavault.org/public arrakis main
+# deb http://downloads.sourceforge.net/project/openmediavault/packages arrakis main
 ## Uncomment the following line to add software from the proposed repository.
-# deb http://packages.openmediavault.org/public erasmus-proposed main
-# deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus-proposed main
+# deb http://packages.openmediavault.org/public arrakis-proposed main
+# deb http://downloads.sourceforge.net/project/openmediavault/packages arrakis-proposed main
 ## This software is not part of OpenMediaVault, but is offered by third-party
 ## developers as a service to OpenMediaVault users.
-# deb http://packages.openmediavault.org/public erasmus partner
-# deb http://downloads.sourceforge.net/project/openmediavault/packages erasmus partner
+# deb http://packages.openmediavault.org/public arrakis partner
+# deb http://downloads.sourceforge.net/project/openmediavault/packages arrakis partner
 EOF
 
 export LANG=C
