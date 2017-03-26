6181b4486d0ec796ff5c765d1aec4e6b249c8da1
votdev <votdev@gmx.de>
2017-03-26 19:25:02 +0200

    Improve the datamodel to be able to resolve shared folder references.

---
+++ deb/openmediavault-clamav/usr/share/openmediavault/confdb/migrations.d/conf.service.clamav_3.3.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2017 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+sfref=$(omv_config_get "/config/services/clamav/quarantine_sharedfolderref")
+omv_config_add_key "/config/services/clamav" "quarantine" ""
+omv_config_add_key "/config/services/clamav/quarantine" "sharedfolderref" "${sfref}"
+omv_config_delete "/config/services/clamav/quarantine_sharedfolderref"
+
+exit 0
