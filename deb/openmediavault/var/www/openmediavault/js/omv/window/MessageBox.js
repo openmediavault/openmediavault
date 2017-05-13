17157ebbd248e2cb8c7b65d4e49a879d62e12ef2
Volker Theile <votdev@gmx.de>
2017-05-13 20:44:54 +0200

    Improve OMV.window.MessageBox.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/var/www/openmediavault/js/omv/window/MessageBox.js
+++ deb/openmediavault/var/www/openmediavault/js/omv/window/MessageBox.js
@@ -121,24 +121,28 @@ Ext.define("OMV.window.MessageBox", {
 	 * @param scope (optional) The scope in which the callback is executed.
 	 */
 	error: function(title, error, fn, scope) {
-		if(!Ext.isObject(error)) {
+		var me = this;
+		if (!Ext.isObject(error)) {
 			this.failure(title, error);
 			return;
 		}
-		var me = this, defaultHeight = 120;
+		// Build the details text.
+		var detailsText = "";
+		if (!Ext.isEmpty(error.code))
+			detailsText += _("Error #") + error.code + ":\n";
+		if (!Ext.isEmpty(error.trace))
+			detailsText += error.trace;
+		detailsText = OMV.util.Format.whitespace(detailsText, "pre");
+		// Display the error dialog.
 		var dlg = Ext.create("Ext.Window", {
-			border: false,
 			title: title || _("Error"),
 			modal: true,
 			width: 400,
-			height: defaultHeight,
 			minHeight: 100,
-			plain: true,
 			resizable: true,
 			constrain: true,
 			minimizable: false,
 			maximizable: false,
-			stateful: false,
 			closable: true,
 			layout: {
 				type: "vbox",
@@ -152,29 +156,20 @@ Ext.define("OMV.window.MessageBox", {
 					// Close the dialog.
 					dlg.close();
 					// Execute given callback function.
-					if(Ext.isFunction(fn))
+					if (Ext.isFunction(fn))
 						fn.call(scope || me, me);
 				}
 			},{
 				text: _("Show details"),
-				hidden: Ext.isEmpty(error.code) &&
-				  Ext.isEmpty(error.trace),
+				hidden: Ext.isEmpty(detailsText),
 				handler: function(c, e) {
-					var text = "";
-					if(!Ext.isEmpty(error.code))
-						text += _("Error #") + error.code + ":\n";
-					if(!Ext.isEmpty(error.trace))
-						text += error.trace;
-					details.update(OMV.util.Format.whitespace(text), "pre");
-					if(details.isVisible()) {
+					if (details.isVisible()) {
 						details.hide();
 						c.setText(_("Show details"));
 					} else {
 						details.show();
 						c.setText(_("Hide details"));
 					}
-					// Recalculate dialog height.
-					dlg.setHeight(null);
 				}
 			}],
 			items: [{
@@ -205,8 +200,8 @@ Ext.define("OMV.window.MessageBox", {
 							me.baseCls + "-text"
 						],
 						html: (Ext.isEmpty(error.message) ?
-						  _("An error has occured") :
-						  Ext.util.Format.htmlEncode(error.message))
+							_("An error has occured") :
+							Ext.util.Format.htmlEncode(error.message))
 					}]
 				}]
 			}, details = Ext.create("Ext.panel.Panel", {
@@ -214,7 +209,8 @@ Ext.define("OMV.window.MessageBox", {
 				flex: 1,
 				hidden: true,
 				scrollable: true,
-				height: 150
+				height: 150,
+				html: detailsText
 			})]
 		});
 		return dlg.show();
