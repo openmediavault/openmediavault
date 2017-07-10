de3a331131fb5f37e41b9dfb7268da31fe97dfc5
Volker Theile <votdev@gmx.de>
2017-07-10 16:57:14 +0200

    Mantis 0001773: UID_MAX and GID_MAX from login.defs not read.
    (cherry picked from commit 77f9934f8bff70b2a5e6d45a5caf3d9b0d242826)
    Signed-off-by: Volker Theile <votdev@gmx.de>

---
+++ deb/openmediavault/usr/share/openmediavault/unittests/php/test_openmediavault_util_keyvaluefile.php
@@ -0,0 +1,43 @@
+#!/usr/bin/phpunit -c/etc/openmediavault
+<?php
+/**
+ * This file is part of OpenMediaVault.
+ *
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @copyright Copyright (c) 2009-2017 Volker Theile
+ *
+ * OpenMediaVault is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OpenMediaVault is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once("openmediavault/functions.inc");
+
+class test_openmediavault_util_keyvaluefile extends \PHPUnit_Framework_TestCase {
+	public function test_space_keyvalue_delimiter() {
+		$file = new \OMV\Util\KeyValueFile(sprintf(
+			"%s/../data/login.defs", getcwd()));
+		$file->setKeyValueDelimiter("\\s");
+		$file->setKeyCaseSensitiv(TRUE);
+		$dict = $file->getAssoc();
+		// Check if the returned value is an associative array.
+		$this->assertInternalType("array", $dict);
+		// Check if some keys exist.
+		$this->assertArrayHasKey("UID_MIN", $dict);
+		$this->assertEquals($dict["UID_MIN"], "1000");
+		$this->assertArrayHasKey("GID_MAX", $dict);
+		$this->assertEquals($dict["GID_MAX"], "60000");
+		$this->assertArrayHasKey("ENCRYPT_METHOD", $dict);
+		$this->assertEquals($dict["ENCRYPT_METHOD"], "SHA512");
+		$this->assertArrayNotHasKey("FAKE_SHELL", $dict);
+	}
+}
