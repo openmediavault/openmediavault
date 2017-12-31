a149fd7b5a37cdd1af88ed298a0b46bfa9059147
Volker Theile <votdev@gmx.de>
2017-12-31 12:56:23 +0100

    Mantis 0001079: Disk IO graphs in 'Diagnostics | System Information'
    Signed-off-by: Volker Theile <votdev@gmx.de>

---
+++ deb/openmediavault-diskstats/usr/share/openmediavault/mkrrdgraph/plugins.d/disk.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2017 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+import openmediavault.mkrrdgraph
+import openmediavault.subprocess
+import os
+
+class Plugin(openmediavault.mkrrdgraph.IPlugin):
+	def create_graph(self, config):
+		# http://paletton.com/#uid=33r0-0kwi++bu++hX++++rd++kX
+		config.update({
+			'title_disk_octets': 'Disk Traffic',
+			'title_disk_ops': 'Disk Operations',
+			'title_disk_time': 'Disk time per operation',
+			'color_line_disk_read': '#0bb6ff', # blue
+			'color_line_disk_write': '#ff1300' # red
+		})
+		# Get the disk devices from the collectd configuration file.
+		devnames = openmediavault.mkrrdgraph.load_collectd_config('disk', 'Disk')
+		for devname in devnames:
+			config['devname'] = devname
+
+			image_filename = '{image_dir}/disk-octets-{devname}-{period}.png'.format(**config)
+			if os.path.exists('{data_dir}/disk-{devname}/disk_octets.rrd'.format(**config)):
+				args = []
+				args.append(image_filename)
+				args.extend(config['defaults'])
+				args.extend(['--start', config['start']])
+				args.extend(['--title', '"{devname} {title_disk_octets}"'.format(**config)])
+				args.append('--slope-mode')
+				args.extend(['--lower-limit', '0'])
+				args.extend(['--vertical-label', '"Bytes per second"'])
+				args.append('DEF:ravg={data_dir}/disk-{devname}/disk_octets.rrd:read:AVERAGE'.format(**config))
+				args.append('DEF:rmin={data_dir}/disk-{devname}/disk_octets.rrd:read:MIN'.format(**config))
+				args.append('DEF:rmax={data_dir}/disk-{devname}/disk_octets.rrd:read:MAX'.format(**config))
+				args.append('DEF:wavg={data_dir}/disk-{devname}/disk_octets.rrd:write:AVERAGE'.format(**config))
+				args.append('DEF:wmin={data_dir}/disk-{devname}/disk_octets.rrd:write:MIN'.format(**config))
+				args.append('DEF:wmax={data_dir}/disk-{devname}/disk_octets.rrd:write:MAX'.format(**config))
+				args.append('LINE1:ravg{color_line_disk_read}:"Read"'.format(**config))
+				args.append('GPRINT:rmin:MIN:"%5.1lf%s Min"'.format(**config))
+				args.append('GPRINT:ravg:AVERAGE:"%5.1lf%s Avg"'.format(**config))
+				args.append('GPRINT:rmax:MAX:"%5.1lf%s Max"'.format(**config))
+				args.append('GPRINT:ravg:LAST:"%5.1lf%s Last\l"'.format(**config))
+				args.append('LINE1:wavg{color_line_disk_write}:"Write"'.format(**config))
+				args.append('GPRINT:wmin:MIN:"%5.1lf%s Min"'.format(**config))
+				args.append('GPRINT:wavg:AVERAGE:"%5.1lf%s Avg"'.format(**config))
+				args.append('GPRINT:wmax:MAX:"%5.1lf%s Max"'.format(**config))
+				args.append('GPRINT:wavg:LAST:"%5.1lf%s Last\l" '.format(**config))
+				args.append('COMMENT:"{last_update}"'.format(**config))
+				openmediavault.mkrrdgraph.call_rrdtool_graph(args)
+			else:
+				openmediavault.mkrrdgraph.copy_placeholder_image(image_filename)
+
+			image_filename = '{image_dir}/disk-ops-{devname}-{period}.png'.format(**config)
+			if os.path.exists('{data_dir}/disk-{devname}/disk_ops.rrd'.format(**config)):
+				args = []
+				args.append(image_filename)
+				args.extend(config['defaults'])
+				args.extend(['--start', config['start']])
+				args.extend(['--title', '"{devname} {title_disk_ops}"'.format(**config)])
+				args.append('--slope-mode')
+				args.extend(['--lower-limit', '0'])
+				args.extend(['--vertical-label', '"Operations per second"'])
+				args.append('DEF:ravg={data_dir}/disk-{devname}/disk_ops.rrd:read:AVERAGE'.format(**config))
+				args.append('DEF:rmin={data_dir}/disk-{devname}/disk_ops.rrd:read:MIN'.format(**config))
+				args.append('DEF:rmax={data_dir}/disk-{devname}/disk_ops.rrd:read:MAX'.format(**config))
+				args.append('DEF:wavg={data_dir}/disk-{devname}/disk_ops.rrd:write:AVERAGE'.format(**config))
+				args.append('DEF:wmin={data_dir}/disk-{devname}/disk_ops.rrd:write:MIN'.format(**config))
+				args.append('DEF:wmax={data_dir}/disk-{devname}/disk_ops.rrd:write:MAX'.format(**config))
+				args.append('LINE1:ravg{color_line_disk_read}:"Read"'.format(**config))
+				args.append('GPRINT:rmin:MIN:"%5.1lf%s Min"'.format(**config))
+				args.append('GPRINT:ravg:AVERAGE:"%5.1lf%s Avg"'.format(**config))
+				args.append('GPRINT:rmax:MAX:"%5.1lf%s Max"'.format(**config))
+				args.append('GPRINT:ravg:LAST:"%5.1lf%s Last\l"'.format(**config))
+				args.append('LINE1:wavg{color_line_disk_write}:"Write"'.format(**config))
+				args.append('GPRINT:wmin:MIN:"%5.1lf%s Min"'.format(**config))
+				args.append('GPRINT:wavg:AVERAGE:"%5.1lf%s Avg"'.format(**config))
+				args.append('GPRINT:wmax:MAX:"%5.1lf%s Max"'.format(**config))
+				args.append('GPRINT:wavg:LAST:"%5.1lf%s Last\l" '.format(**config))
+				args.append('COMMENT:"{last_update}"'.format(**config))
+				openmediavault.mkrrdgraph.call_rrdtool_graph(args)
+			else:
+				openmediavault.mkrrdgraph.copy_placeholder_image(image_filename)
+
+			image_filename = '{image_dir}/disk-time-{devname}-{period}.png'.format(**config)
+			if os.path.exists('{data_dir}/disk-{devname}/disk_time.rrd'.format(**config)):
+				args = []
+				args.append(image_filename)
+				args.extend(config['defaults'])
+				args.extend(['--start', config['start']])
+				args.extend(['--title', '"{devname} {title_disk_time}"'.format(**config)])
+				args.append('--slope-mode')
+				args.extend(['--lower-limit', '0'])
+				args.extend(['--vertical-label', '"Avg. Time/Op"'])
+				args.append('DEF:ravg={data_dir}/disk-{devname}/disk_time.rrd:read:AVERAGE'.format(**config))
+				args.append('DEF:rmin={data_dir}/disk-{devname}/disk_time.rrd:read:MIN'.format(**config))
+				args.append('DEF:rmax={data_dir}/disk-{devname}/disk_time.rrd:read:MAX'.format(**config))
+				args.append('DEF:wavg={data_dir}/disk-{devname}/disk_time.rrd:write:AVERAGE'.format(**config))
+				args.append('DEF:wmin={data_dir}/disk-{devname}/disk_time.rrd:write:MIN'.format(**config))
+				args.append('DEF:wmax={data_dir}/disk-{devname}/disk_time.rrd:write:MAX'.format(**config))
+				args.append('LINE1:ravg{color_line_disk_read}:"Read"'.format(**config))
+				args.append('GPRINT:rmin:MIN:"%5.1lf%ss Min"'.format(**config))
+				args.append('GPRINT:ravg:AVERAGE:"%5.1lf%ss Avg"'.format(**config))
+				args.append('GPRINT:rmax:MAX:"%5.1lf%ss Max"'.format(**config))
+				args.append('GPRINT:ravg:LAST:"%5.1lf%ss Last\l"'.format(**config))
+				args.append('LINE1:wavg{color_line_disk_write}:"Write"'.format(**config))
+				args.append('GPRINT:wmin:MIN:"%5.1lf%ss Min"'.format(**config))
+				args.append('GPRINT:wavg:AVERAGE:"%5.1lf%ss Avg"'.format(**config))
+				args.append('GPRINT:wmax:MAX:"%5.1lf%ss Max"'.format(**config))
+				args.append('GPRINT:wavg:LAST:"%5.1lf%ss Last\l" '.format(**config))
+				args.append('COMMENT:"{last_update}"'.format(**config))
+				openmediavault.mkrrdgraph.call_rrdtool_graph(args)
+			else:
+				openmediavault.mkrrdgraph.copy_placeholder_image(image_filename)
+		return 0
