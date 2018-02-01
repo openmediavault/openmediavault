73b40b74043fb9a12f3a3ff5c9c02173d5d9468b
Volker Theile <votdev@gmx.de>
2018-02-02 00:02:32 +0100

    Fix WebUI issue in Firefox browser. See
    * https://bugtracker.openmediavault.org/view.php?id=1830
    * https://www.sencha.com/forum/showthread.php?336762-Examples-don-t-work-in-Firefox-52-touchscreen&p=1174857&viewfull=1#post1174857.
    
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/var/www/openmediavault/js/ext-overrides.js
+++ deb/openmediavault/var/www/openmediavault/js/ext-overrides.js
@@ -1039,6 +1039,56 @@ Ext.apply(Ext.String, {
 });
 
 ////////////////////////////////////////////////////////////////////////////////
+// Ext.dom.Element
+////////////////////////////////////////////////////////////////////////////////
+
+// https://www.sencha.com/forum/showthread.php?336762-Examples-don-t-work-in-Firefox-52-touchscreen&p=1174857&viewfull=1#post1174857
+Ext.define('EXTJS_23846.Element', {
+	override: 'Ext.dom.Element'
+}, function(Element) {
+	var supports = Ext.supports,
+		proto = Element.prototype,
+		eventMap = proto.eventMap,
+		additiveEvents = proto.additiveEvents;
+
+	if (Ext.os.is.Desktop && supports.TouchEvents && !supports.PointerEvents) {
+		eventMap.touchstart = 'mousedown';
+		eventMap.touchmove = 'mousemove';
+		eventMap.touchend = 'mouseup';
+		eventMap.touchcancel = 'mouseup';
+
+		additiveEvents.mousedown = 'mousedown';
+		additiveEvents.mousemove = 'mousemove';
+		additiveEvents.mouseup = 'mouseup';
+		additiveEvents.touchstart = 'touchstart';
+		additiveEvents.touchmove = 'touchmove';
+		additiveEvents.touchend = 'touchend';
+		additiveEvents.touchcancel = 'touchcancel';
+
+		additiveEvents.pointerdown = 'mousedown';
+		additiveEvents.pointermove = 'mousemove';
+		additiveEvents.pointerup = 'mouseup';
+		additiveEvents.pointercancel = 'mouseup';
+	}
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Ext.event.publisher.Gesture
+////////////////////////////////////////////////////////////////////////////////
+
+// https://www.sencha.com/forum/showthread.php?336762-Examples-don-t-work-in-Firefox-52-touchscreen&p=1174857&viewfull=1#post1174857
+Ext.define('EXTJS_23846.Gesture', {
+	override: 'Ext.event.publisher.Gesture'
+}, function(Gesture) {
+	var me = Gesture.instance;
+
+	if (Ext.supports.TouchEvents && !Ext.isWebKit && Ext.os.is.Desktop) {
+		me.handledDomEvents.push('mousedown', 'mousemove', 'mouseup');
+		me.registerEvents();
+	}
+});
+
+////////////////////////////////////////////////////////////////////////////////
 // Additional helper functions
 ////////////////////////////////////////////////////////////////////////////////
 
