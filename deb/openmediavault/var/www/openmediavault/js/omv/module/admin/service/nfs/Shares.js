b13ded6a4b31b2b91719923dcaf8e727632a3758
Volker Theile <votdev@gmx.de>
2018-02-11 09:27:37 +0100

    Mantis 0000196: Switch the NFS shares default option from secure to insecure because NFS is running on port 2049 by default.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/var/www/openmediavault/js/omv/module/admin/service/nfs/Shares.js
+++ deb/openmediavault/var/www/openmediavault/js/omv/module/admin/service/nfs/Shares.js
@@ -90,7 +90,7 @@ Ext.define("OMV.module.admin.service.nfs.Share", {
 			name: "extraoptions",
 			fieldLabel: _("Extra options"),
 			allowBlank: true,
-			value: "subtree_check,secure",
+			value: "subtree_check,insecure",
 			plugins: [{
 				ptype: "fieldinfo",
 				text: _("Please check the <a href='http://linux.die.net/man/5/exports' target='_blank'>manual page</a> for more details.")
