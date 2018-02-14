4b04587cfa537f907cca4e5724f5bd1cd92a5081
Volker Theile <votdev@gmx.de>
2018-02-14 17:46:31 +0100

    Update URLs
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/var/www/openmediavault/js/omv/module/public/info/support/Support.js
+++ deb/openmediavault/var/www/openmediavault/js/omv/module/public/info/support/Support.js
@@ -32,11 +32,11 @@ Ext.define("OMV.module.public.info.support.Support", {
 		var me = this;
 		me.html = "<form style='overflow: auto; height: 100%;'>";
 		me.html += me.createBox(
-		  "<b>Project homepage</b><br/><a href='http://www.openmediavault.org' target='_blank'>http://www.openmediavault.org</a><br/><br/>" +
-		  "<b>Forum</b><br/><a href='http://forum.openmediavault.org' target='_blank'>http://forum.openmediavault.org</a><br/><br/>" +
-		  "<b>Documentation</b><br/><a href='http://docs.openmediavault.org' target='_blank'>http://docs.openmediavault.org</a><br/><br/>" +
-		  "<b>API documentation</b><br/><a href='http://apidocs.openmediavault.org' target='_blank'>http://apidocs.openmediavault.org</a><br/><br/>" +
-		  "<b>Bugtracker</b><br/><a href='http://bugtracker.openmediavault.org' target='_blank'>http://bugtracker.openmediavault.org</a><br/><br/>" +
+		  "<b>Project homepage</b><br/><a href='https://www.openmediavault.org' target='_blank'>https://www.openmediavault.org</a><br/><br/>" +
+		  "<b>Forum</b><br/><a href='https://forum.openmediavault.org' target='_blank'>https://forum.openmediavault.org</a><br/><br/>" +
+		  "<b>Documentation</b><br/><a href='https://docs.openmediavault.org' target='_blank'>https://docs.openmediavault.org</a><br/><br/>" +
+		  "<b>API documentation</b><br/><a href='https://apidocs.openmediavault.org' target='_blank'>https://apidocs.openmediavault.org</a><br/><br/>" +
+		  "<b>Bugtracker</b><br/><a href='https://bugtracker.openmediavault.org' target='_blank'>https://bugtracker.openmediavault.org</a><br/><br/>");
 		me.html += "</form>";
 		me.callParent(arguments);
 	},
