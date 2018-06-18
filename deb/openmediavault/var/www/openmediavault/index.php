6a1abe213553f14f0894beb3cd0dbab07ea0195d
Volker Theile <votdev@gmx.de>
2018-06-18 20:22:37 +0200

    Set default values.
    Signed-off-by: Volker Theile <votdev@gmx.de>
    (cherry picked from commit 94b2ac44b096fc1c984a01878647ec0d1005a8c3)

--- deb/openmediavault/var/www/openmediavault/index.php
+++ deb/openmediavault/var/www/openmediavault/index.php
@@ -24,7 +24,7 @@ try {
 	require_once("openmediavault/env.inc");
 
 	// Display errors if debugging is enabled.
-	if (TRUE === \OMV\Environment::getBoolean("OMV_DEBUG_PHP"))
+	if (TRUE === \OMV\Environment::getBoolean("OMV_DEBUG_PHP", FALSE))
 		ini_set("display_errors", 1);
 
 	$session = &\OMV\Session::getInstance();
