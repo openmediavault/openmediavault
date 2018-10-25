d83643f86eefff7afb60716cf0571584d44a5385
Volker Theile <votdev@gmx.de>
2018-10-25 09:24:32 +0200

    Add more python unit tests.
    (cherry picked from commit ac4fa62e96280d840edad937a96f9b19003b03e5)
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/usr/share/openmediavault/unittests/python/test_openmediavault_subprocess.py
+++ deb/openmediavault/usr/share/openmediavault/unittests/python/test_openmediavault_subprocess.py
@@ -20,14 +20,37 @@
 # along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
 import subprocess
 import unittest
+import mock
 import openmediavault.subprocess
 
 class SubprocessTestCase(unittest.TestCase):
+	def _validate_call_args(self, call_args):
+		kwargs = call_args[1]
+		self.assertIn("env", kwargs)
+		env = kwargs["env"]
+		self.assertIn("LANG", env)
+		self.assertEqual(env["LANG"], "C")
+
 	def test_popen(self):
-		p = openmediavault.subprocess.Popen(["whoami"],
-			stdout=subprocess.PIPE, shell=False)
-		stdout, _ = p.communicate()
-		self.assertEqual(stdout.decode().strip(), "root")
+		with mock.patch("subprocess.Popen.__init__") as mock_call:
+			p = openmediavault.subprocess.Popen(["cat", "/proc/uptime"],
+				stdout=subprocess.PIPE, shell=False)
+			self._validate_call_args(mock_call.call_args_list[0])
+
+	def test_call(self):
+		with mock.patch("subprocess.call") as mock_call:
+			openmediavault.subprocess.call(["whoami"])
+			self._validate_call_args(mock_call.call_args_list[0])
+
+	def test_check_call(self):
+		with mock.patch("subprocess.check_call") as mock_check_call:
+			openmediavault.subprocess.check_call(["ls"])
+			self._validate_call_args(mock_check_call.call_args_list[0])
+
+	def test_check_output(self):
+		with mock.patch("subprocess.check_output") as mock_check_output:
+			openmediavault.subprocess.check_output(["uname"])
+			self._validate_call_args(mock_check_output.call_args_list[0])
 
 if __name__ == "__main__":
 	unittest.main()
