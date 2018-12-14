e6345824ef270c497350593eeb968c1cc15ff1b3
Volker Theile <votdev@gmx.de>
2018-12-14 22:18:47 +0100

    Download and install archive key from package repository.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -44,15 +44,15 @@ EOF
 export LANG=C
 export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
-apt-get update
-apt-get --allow-unauthenticated install openmediavault-keyring
+wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key
+apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc"
 apt-get update
 apt-get --yes --auto-remove --show-upgraded \
 	--allow-downgrades --allow-change-held-packages \
 	--no-install-recommends \
 	--option Dpkg::Options::="--force-confdef" \
 	--option DPkg::Options::="--force-confold" \
-	install postfix openmediavault
+	install postfix openmediavault-keyring openmediavault
 
 # Populate the database.
 omv-confdbadm populate
