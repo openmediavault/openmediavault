b10a578161b5f232dd2dfeed678727e754049959
Volker Theile <votdev@gmx.de>
2019-10-29 09:20:16 +0100

    Improve code.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/srv/salt/_modules/omv_utils.py
+++ deb/openmediavault/srv/salt/_modules/omv_utils.py
@@ -51,14 +51,16 @@ def is_ipv6_enabled():
     :rtype: bool
     """
     result = False
-    s = socket.socket(socket.AF_INET6)
+    s = None
     try:
+        s = socket.socket(socket.AF_INET6)
         s.bind(('::1', 0))
         result = True
     except Exception:
         pass
     finally:
-        s.close()
+        if s:
+            s.close()
     return result
 
 
