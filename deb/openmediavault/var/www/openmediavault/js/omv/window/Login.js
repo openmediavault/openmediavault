8d1fad314fdf01c914ccf94588b40d46bbf18c7b
Abo Khalid <myomv100@gmail.com>
2020-02-03 23:26:30 +0300

    add RTL support
    Signed-off-by: Abo Khalid <myomv100@gmail.com>

--- deb/openmediavault/var/www/openmediavault/js/omv/window/Login.js
+++ deb/openmediavault/var/www/openmediavault/js/omv/window/Login.js
@@ -37,9 +37,11 @@ Ext.define("OMV.window.Login", {
 		"OMV.form.Panel",
 		"OMV.form.field.Password",
 		"OMV.form.field.LanguageComboBox",
-		"OMV.window.MessageBox"
+		"OMV.window.MessageBox",
+		'Ext.rtl.*'
 	],
 
+	rtl: OMV.util.i18n.isRTL(),
 	title: _("Login"),
 	layout: "fit",
 	width: 350,
@@ -47,7 +49,6 @@ Ext.define("OMV.window.Login", {
 	buttonAlign: "center",
 	enableResponsiveness: false,
 	stateful: false,
-
 	constructor: function() {
 		var me = this;
 		me.callParent(arguments);
