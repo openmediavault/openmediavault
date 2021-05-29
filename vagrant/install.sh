8ffb8f772e26bd6500970712f992eedd1e4a9968
Volker Theile <votdev@gmx.de>
2021-05-29 14:25:18 +0200

    - Prepare new version - Uncomment some lines in Vagrant install script
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -59,15 +59,15 @@ deb http://packages.openmediavault.org/public shaitan main
 # deb http://downloads.sourceforge.net/project/openmediavault/packages shaitan partner
 EOF
 apt-get update
-# apt-get --yes --auto-remove --show-upgraded \
-# 	--allow-downgrades --allow-change-held-packages \
-# 	--no-install-recommends \
-# 	--option Dpkg::Options::="--force-confdef" \
-# 	--option DPkg::Options::="--force-confold" \
-# 	install openmediavault-keyring openmediavault
+apt-get --yes --auto-remove --show-upgraded \
+	--allow-downgrades --allow-change-held-packages \
+	--no-install-recommends \
+	--option Dpkg::Options::="--force-confdef" \
+	--option DPkg::Options::="--force-confold" \
+	install openmediavault-keyring openmediavault
 
 # Populate the database.
-# omv-confdbadm populate
+omv-confdbadm populate
 
 # Display the login information.
 cat /etc/issue
