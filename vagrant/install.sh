c9fffd853b76d9f6a57bc5b3d71094f9be2b1475
Volker Theile <votdev@gmx.de>
2021-07-27 13:16:11 +0200

    Fix typo.
    Signed-off-by: Volker Theile <votdev@gmx.de>
    (cherry picked from commit 77f458aa878343fee476f0fc44bf7a7c59ce3c19)
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -50,7 +50,7 @@ apt-get update
 apt-get --yes --auto-remove --show-upgraded \
 	--allow-downgrades --allow-change-held-packages \
 	--no-install-recommends \
-	--option Dpkg::Options::="--force-confdef" \
+	--option DPkg::Options::="--force-confdef" \
 	--option DPkg::Options::="--force-confold" \
 	install openmediavault-keyring openmediavault
 
