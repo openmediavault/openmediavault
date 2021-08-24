11d71b761ac15d1de124c2be774ddd2e91eb96c1
Volker Theile <votdev@gmx.de>
2021-08-24 09:58:21 +0200

    Fix unit test.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/shared/pipes/locale-date.pipe.spec.ts
+++ deb/openmediavault/workbench/src/app/shared/pipes/locale-date.pipe.spec.ts
@@ -5,27 +5,39 @@ import { LocaleDatePipe } from '~/app/shared/pipes/locale-date.pipe';
 describe('LocaleDatePipe', () => {
   const pipe = new LocaleDatePipe();
 
+  beforeEach(() => {
+    dayjs.locale('de');
+  });
+
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
   it('transform date into human readable time (1)', () => {
-    const date: Date = dayjs().subtract(130, 'seconds').toDate();
+    const date: Date = dayjs()
+      .subtract(130, 'seconds')
+      .toDate();
     expect(pipe.transform(date, 'relative')).toBe('2 minutes ago');
   });
 
   it('transform date into human readable time (2)', () => {
-    const date: string = dayjs().subtract(40, 'seconds').toISOString();
+    const date: string = dayjs()
+      .subtract(40, 'seconds')
+      .toISOString();
     expect(pipe.transform(date, 'relative')).toBe('a few seconds ago');
   });
 
   it('transform date into human readable time (3)', () => {
-    const date: Date = dayjs().subtract(70, 'minutes').toDate();
+    const date: Date = dayjs()
+      .subtract(70, 'minutes')
+      .toDate();
     expect(pipe.transform(date, 'relative')).toBe('an hour ago');
   });
 
   it('transform date into human readable time (4)', () => {
-    const date: number = dayjs().subtract(3, 'days').unix();
+    const date: number = dayjs()
+      .subtract(3, 'days')
+      .unix();
     expect(pipe.transform(date, 'relative')).toBe('3 days ago');
   });
 
@@ -36,16 +48,18 @@ describe('LocaleDatePipe', () => {
 
   it('transform date into human readable time (6)', () => {
     const date: Date = dayjs('2013-02-04T22:44:30.652Z').toDate();
-    expect(pipe.transform(date, 'datetime')).toBe('2013-2-4 23:44:30');
+    expect(pipe.transform(date, 'datetime')).toBe('4.2.2013, 23:44:30');
   });
 
   it('transform date into human readable time (7)', () => {
     const date: Date = dayjs('2013-02-04T22:44:30.652Z').toDate();
-    expect(pipe.transform(date, 'date')).toBe('2013-2-4');
+    expect(pipe.transform(date, 'date')).toBe('4.2.2013');
   });
 
   it('transform date into human readable time (8)', () => {
-    const date: Date = dayjs().subtract(70, 'minutes').toDate();
+    const date: Date = dayjs()
+      .subtract(70, 'minutes')
+      .toDate();
     expect(pipe.transform(date, 'relative', true)).toBe('an hour');
   });
 });
