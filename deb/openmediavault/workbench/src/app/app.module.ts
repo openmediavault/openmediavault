cb4b535fdf71f53a21bb786f28fd47b1f5f29f38
Volker Theile <votdev@gmx.de>
2021-08-31 21:49:02 +0200

    Fix DI issue. The constructor of the SystemInformationService service was called to late sometimes.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/app.module.ts
+++ deb/openmediavault/workbench/src/app/app.module.ts
@@ -34,7 +34,6 @@ import { MaterialModule } from '~/app/material.module';
 import { GlobalErrorHandlerService } from '~/app/shared/services/global-error-handler.service';
 import { HttpErrorInterceptorService } from '~/app/shared/services/http-error-interceptor.service';
 import { LocaleService } from '~/app/shared/services/locale.service';
-import { SystemInformationService } from '~/app/shared/services/system-information.service';
 import { SharedModule } from '~/app/shared/shared.module';
 
 @NgModule({
@@ -60,7 +59,6 @@ import { SharedModule } from '~/app/shared/shared.module';
     AppRoutingModule
   ],
   providers: [
-    SystemInformationService,
     {
       provide: ErrorHandler,
       useClass: GlobalErrorHandlerService
