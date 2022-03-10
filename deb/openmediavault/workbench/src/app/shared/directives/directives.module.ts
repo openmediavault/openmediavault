45eb929f28210b8e325b4c5ae8c0fbef3304172a
Volker Theile <votdev@gmx.de>
2022-03-10 11:06:18 +0100

    Show image loading status.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/shared/directives/directives.module.ts
+++ deb/openmediavault/workbench/src/app/shared/directives/directives.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AutocapitalizeDirective } from '~/app/shared/directives/autocapitalize.directive';
 import { AutofocusDirective } from '~/app/shared/directives/autofocus.directive';
 import { BinaryUnitDirective } from '~/app/shared/directives/binary-unit.directive';
+import { LoadingStateDirective } from '~/app/shared/directives/loading-state.directive';
 import { NativeElementDirective } from '~/app/shared/directives/native-element.directive';
 import { StickyDirective } from '~/app/shared/directives/sticky.directive';
 
@@ -13,14 +14,16 @@ import { StickyDirective } from '~/app/shared/directives/sticky.directive';
     AutofocusDirective,
     NativeElementDirective,
     StickyDirective,
-    BinaryUnitDirective
+    BinaryUnitDirective,
+    LoadingStateDirective
   ],
   exports: [
     AutocapitalizeDirective,
     AutofocusDirective,
     NativeElementDirective,
     StickyDirective,
-    BinaryUnitDirective
+    BinaryUnitDirective,
+    LoadingStateDirective
   ],
   imports: [CommonModule]
 })
---
+++ deb/openmediavault/workbench/src/app/shared/directives/loading-state.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+/**
+ * A directive that adds the classes 'loading', 'loaded' or 'error'
+ * to the image element depending on the loading state.
+ */
+@Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  selector: '[loadingState]'
+})
+export class LoadingStateDirective {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('loadstart') onLoadStart() {
+    this.renderer.addClass(this.elementRef.nativeElement, 'loading');
+  }
+
+  @HostListener('load') onLoad() {
+    this.renderer.removeClass(this.elementRef.nativeElement, 'loading');
+    this.renderer.addClass(this.elementRef.nativeElement, 'loaded');
+  }
+
+  @HostListener('error') onError() {
+    this.renderer.removeClass(this.elementRef.nativeElement, 'loading');
+    this.renderer.addClass(this.elementRef.nativeElement, 'error');
+  }
+}
