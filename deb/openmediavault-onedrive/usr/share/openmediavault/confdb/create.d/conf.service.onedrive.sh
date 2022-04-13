319341eb7703d018648b29a16369222ca9ed2938
Volker Theile <votdev@gmx.de>
2022-04-13 17:14:05 +0200

    Add OneDrive plugin. (#1276)
    Signed-off-by: Volker Theile <votdev@gmx.de>

---
+++ deb/openmediavault-onedrive/usr/share/openmediavault/confdb/create.d/conf.service.onedrive.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2022 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+########################################################################
+# Update the configuration.
+# <config>
+#   <services>
+#     <onedrive>
+#       <enable>0|1</enable>
+#       <username></username>
+#       <sharedfolderref></sharedfolderref>
+#     </onedrive>
+#   </services>
+# </config>
+########################################################################
+if ! omv_config_exists "/config/services/onedrive"; then
+	omv_config_add_node "/config/services" "onedrive"
+	omv_config_add_key "/config/services/onedrive" "enable" "0"
+	omv_config_add_key "/config/services/onedrive" "username" ""
+	omv_config_add_key "/config/services/onedrive" "sharedfolderref" ""
+fi
+
+exit 0
