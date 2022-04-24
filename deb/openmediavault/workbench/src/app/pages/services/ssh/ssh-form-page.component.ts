34a59ae20f6e5ab8d2cd1e4defc4642267b420d8
Volker Theile <votdev@gmx.de>
2022-04-24 17:25:11 +0200

    Add hint.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/pages/services/ssh/ssh-form-page.component.ts
+++ deb/openmediavault/workbench/src/app/pages/services/ssh/ssh-form-page.component.ts
@@ -36,6 +36,14 @@ export class SshFormPageComponent {
     },
     fields: [
       {
+        type: 'hint',
+        text: gettext(
+          'Users must be assigned to the <em>ssh</em> group to be able to log in via SSH.'
+        ),
+        dismissible: true,
+        stateId: '1f7e0754-e049-4578-9272-8cbb365fad97'
+      },
+      {
         type: 'checkbox',
         name: 'enable',
         label: gettext('Enabled'),
