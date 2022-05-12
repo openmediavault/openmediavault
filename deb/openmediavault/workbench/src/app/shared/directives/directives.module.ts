65d144bb7783aed43b957f8d8f3e383fbd41c28a
Volker Theile <votdev@gmx.de>
2022-05-12 17:48:34 +0200

    Cleanup sticky implementation.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/shared/directives/directives.module.ts
+++ deb/openmediavault/workbench/src/app/shared/directives/directives.module.ts
@@ -6,14 +6,12 @@ import { AutofocusDirective } from '~/app/shared/directives/autofocus.directive'
 import { BinaryUnitDirective } from '~/app/shared/directives/binary-unit.directive';
 import { LoadingStateDirective } from '~/app/shared/directives/loading-state.directive';
 import { NativeElementDirective } from '~/app/shared/directives/native-element.directive';
-import { StickyDirective } from '~/app/shared/directives/sticky.directive';
 
 @NgModule({
   declarations: [
     AutocapitalizeDirective,
     AutofocusDirective,
     NativeElementDirective,
-    StickyDirective,
     BinaryUnitDirective,
     LoadingStateDirective
   ],
@@ -21,7 +19,6 @@ import { StickyDirective } from '~/app/shared/directives/sticky.directive';
     AutocapitalizeDirective,
     AutofocusDirective,
     NativeElementDirective,
-    StickyDirective,
     BinaryUnitDirective,
     LoadingStateDirective
   ],
--- deb/openmediavault/workbench/src/app/shared/directives/sticky.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StickyDirective } from '~/app/shared/directives/sticky.directive';
-
-describe('StickyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new StickyDirective(null);
-    expect(directive).toBeTruthy();
-  });
-});
