3f8662793a4d38832f8391ad4dba50e58891ff6d
Volker Theile <votdev@gmx.de>
2022-12-29 15:23:06 +0100

    Change 'control panel' to 'workbench' in omv-firstaid.
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/usr/share/openmediavault/firstaid/modules.d/30reset_webgui_passwd.py
+++ deb/openmediavault/usr/share/openmediavault/firstaid/modules.d/30reset_webgui_passwd.py
@@ -28,7 +28,7 @@ import openmediavault.rpc
 class Module(openmediavault.firstaid.IModule):
     @property
     def description(self):
-        return "Change control panel administrator password"
+        return "Change workbench administrator password"
 
     def execute(self):
         d = dialog.Dialog(dialog="dialog")
@@ -78,14 +78,14 @@ class Module(openmediavault.firstaid.IModule):
                     height=5,
                     width=30,
                 )
-        print("Updating control panel administrator password. Please wait ...")
+        print("Updating workbench administrator password. Please wait ...")
         openmediavault.rpc.call(
             "UserMgmt", "setPasswordByContext", {"password": password}
         )
         # openmediavault.rpc.call("Config", "applyChanges",
         # 	{ "modules": [], "force": False })
         print(
-            "The control panel administrator password was successfully "
+            "The workbench administrator password was successfully "
             "changed."
         )
         return 0
