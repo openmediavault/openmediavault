001a8b58f95713fda378663d6ce8d51c75850f10
Volker Theile <votdev@gmx.de>
2023-09-07 08:50:03 +0200

    Display hostname in browser window title only
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/shared/services/title.service.ts
+++ deb/openmediavault/workbench/src/app/shared/services/title.service.ts
@@ -1,11 +1,9 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Event, NavigationEnd, Router } from '@angular/router';
-import * as _ from 'lodash';
 import { Subscription } from 'rxjs';
-import { filter, switchMap } from 'rxjs/operators';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 
-import { translate } from '~/app/i18n.helper';
+import { Unsubscribe } from '~/app/decorators';
 import {
   SystemInformation,
   SystemInformationService
@@ -16,48 +14,21 @@ const DEFAULT_TITLE = 'openmediavault Workbench';
 @Injectable({
   providedIn: 'root'
 })
-export class TitleService implements OnDestroy {
-  private subscription: Subscription;
+export class TitleService {
+  @Unsubscribe()
+  private subscriptions = new Subscription();
 
-  constructor(
-    private router: Router,
-    private systemInformationService: SystemInformationService,
-    private title: Title
-  ) {
-    this.subscription = this.router.events
-      .pipe(
-        filter((event: Event) => event instanceof NavigationEnd),
-        switchMap(() => {
-          return this.systemInformationService.systemInfo$;
+  constructor(private systemInformationService: SystemInformationService, private title: Title) {
+    this.subscriptions.add(
+      this.systemInformationService.systemInfo$
+        .pipe(
+          map((resp: SystemInformation): string => resp.hostname),
+          distinctUntilChanged()
+        )
+        .subscribe((hostname: string) => {
+          const newTitle = `${hostname} - ${DEFAULT_TITLE}`;
+          this.title.setTitle(newTitle);
         })
-      )
-      .subscribe((res: SystemInformation) => {
-        const titles: string[] = this.getTitles(this.router.routerState.root);
-        const text: string = titles.join(' | ');
-        const newTitle =
-          (res.hostname.length ? `${res.hostname} - ` : '') +
-          `${DEFAULT_TITLE}${titles.length ? ' - ' : ''}${text}`;
-        // Use the following template for the page title:
-        // (<HOSTNAME> - )?<DEFAULT_TITLE>( - <BREADCRUMBS>)?
-        this.title.setTitle(newTitle);
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  private getTitles(route: ActivatedRoute): string[] {
-    const titles: string[] = [];
-    while (route?.firstChild) {
-      if (route.snapshot.data.title) {
-        titles.push(route.snapshot.data.title);
-      }
-      route = route.firstChild;
-    }
-    if (route.snapshot.data.title) {
-      titles.push(route.snapshot.data.title);
-    }
-    return _.uniqWith(titles, _.isEqual).map((title) => translate(title));
+    );
   }
 }
