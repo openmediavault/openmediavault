97aa3735aaedf48658cca1687118e4a185d17baa
Volker Theile <votdev@gmx.de>
2024-02-27 15:10:08 +0100

    Fix unittest
    Fixes: https://github.com/openmediavault/openmediavault/commit/8437338e64890bdd955f535b0fac23ca078b89ab
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/shared/services/rpc.service.spec.ts
+++ deb/openmediavault/workbench/src/app/shared/services/rpc.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ToastrModule } from 'ngx-toastr';
 
 import { RpcService } from '~/app/shared/services/rpc.service';
 import { TestingModule } from '~/app/testing.module';
@@ -6,7 +7,7 @@ import { TestingModule } from '~/app/testing.module';
 describe('RpcService', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [TestingModule]
+      imports: [TestingModule, ToastrModule.forRoot()]
     })
   );
 
