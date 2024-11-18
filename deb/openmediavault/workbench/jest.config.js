3da106f2748adaefa680c4485a539b6417830433
Volker Theile <votdev@gmx.de>
2024-11-18 08:36:17 +0100

    Migrate other packages
    References:
    - https://v15.angular.io/guide/versions
    - https://github.com/siemens/ngx-datatable/blob/master/docs/changelog.md#2160
    - https://jsverse.github.io/transloco/docs/getting-started/angular-compatibility
    - https://github.com/MurhafSousli/ngx-scrollbar/blob/master/CHANGELOG.md#1100
    - https://www.npmjs.com/package/ngx-toastr
    
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/jest.config.js
+++ deb/openmediavault/workbench/jest.config.js
@@ -1,3 +1,4 @@
+// https://thymikee.github.io/jest-preset-angular/docs
 const esModules = ['@codemirror/legacy-modes', 'ansi-regex', 'rxjs', 'strip-ansi'];
 module.exports = {
   testTimeout: 10000,
@@ -5,10 +6,7 @@ module.exports = {
     '~/(.*)$': '<rootDir>/src/$1'
   },
   preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
   setupFiles: ['jest-canvas-mock'],
   setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
-  testMatch: ['**/*.spec.ts'],
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|'.concat(esModules.join('|'), ')')],
-  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs']
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|'.concat(esModules.join('|'), ')')]
 };
