5c8f98156b5d88329662963a5a8dabf64c43194c
Volker Theile <votdev@gmx.de>
2024-11-19 20:47:59 +0100

    Several changes
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault-bcache/usr/share/openmediavault/confdb/create.d/conf.service.bcache.sh
+++ deb/openmediavault-bcache/usr/share/openmediavault/confdb/create.d/conf.service.bcache.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env dash
+#
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2024 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
 
 set -e
 
@@ -9,3 +28,5 @@ if ! omv_config_exists "/config/services/bcache"; then
 	omv_config_add_node "/config/services/bcache" "caches"
 	omv_config_add_node "/config/services/bcache" "backings"
 fi
+
+exit 0
