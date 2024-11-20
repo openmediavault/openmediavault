63de3b9901dccd6cb0558fa8418883bfc904a572
myrck <20473294+myrck@users.noreply.github.com>
2024-11-20 13:24:07 +0100

    Add bcache plugin (#1853)
    * Add bcache filesystem backend
    
    Preparation for the bcache plugin and to avoid bcache formatted devices as candidates for various plugins such as filesystem and lvm.
    
    Signed-off-by: Michael Myrcik <michael.myrcik@web.de>
    
    * Add Bcache plugin
    
    Makes it possible to use an SSD as a read/write cache for another blockdevice.
    
    Fixes: #221
    
    Signed-off-by: Michael Myrcik <michael.myrcik@web.de>
    
    Co-authored-by: Volker Theile <votdev@gmx.de>
    
    * Several changes
    
    Signed-off-by: Volker Theile <votdev@gmx.de>
    
    ---------
    
    Signed-off-by: Michael Myrcik <michael.myrcik@web.de>
    Signed-off-by: Volker Theile <votdev@gmx.de>
    Co-authored-by: Volker Theile <votdev@gmx.de>

---
+++ deb/openmediavault-bcache/srv/salt/omv/deploy/bcache/default.sls
@@ -0,0 +1,27 @@
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2024 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+
+{% set dirpath = '/srv/salt' | path_join(tpldir) %}
+
+include:
+{% for file in salt['file.readdir'](dirpath) | sort %}
+{% if file | regex_match('^(\d+.+).sls$', ignorecase=True) %}
+  - .{{ file | replace('.sls', '') }}
+{% endif %}
+{% endfor %}
---
+++ deb/openmediavault-bcache/usr/share/openmediavault/confdb/create.d/conf.service.bcache.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env dash
+#
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2024 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+if ! omv_config_exists "/config/services/bcache"; then
+	omv_config_add_node "/config/services" "bcache"
+	omv_config_add_node "/config/services/bcache" "caches"
+	omv_config_add_node "/config/services/bcache" "backings"
+fi
+
+exit 0
