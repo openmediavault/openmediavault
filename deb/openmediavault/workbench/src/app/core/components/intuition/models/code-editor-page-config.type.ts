ce0b2f381ce45f0b11f82d0f801e983988b8f4bf
Volker Theile <votdev@gmx.de>
2025-01-01 13:16:52 +0100

    Update copyright
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- deb/openmediavault/workbench/src/app/core/components/intuition/models/code-editor-page-config.type.ts
+++ deb/openmediavault/workbench/src/app/core/components/intuition/models/code-editor-page-config.type.ts
@@ -5,7 +5,7 @@ import { TextPageButtonConfig } from '~/app/core/components/intuition/models/tex
  *
  * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
  * @author    Volker Theile <volker.theile@openmediavault.org>
- * @copyright Copyright (c) 2009-2024 Volker Theile
+ * @copyright Copyright (c) 2009-2025 Volker Theile
  *
  * OpenMediaVault is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
