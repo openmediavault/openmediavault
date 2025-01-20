75cd3fe9fe39d65eb919367a0d13f7cc12f44efb
Volker Theile <votdev@gmx.de>
2025-01-20 19:41:41 +0100

    Fix database migration issue introduced by issue #1875
    Signed-off-by: Volker Theile <votdev@gmx.de>

---
+++ deb/openmediavault/usr/share/openmediavault/confdb/migrations.d/conf_7.5.2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env dash
+#
+# This file is part of OpenMediaVault.
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2025 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+# Note, this should already be part of 7.5.0, but the PR didn't use the
+# correct version number for the migration script. So we add it here
+# again to make sure the database objects are migrated properly.
+omv_config_add_key "/config/system/network/interfaces/interface" "band" "auto"
+
+exit 0
