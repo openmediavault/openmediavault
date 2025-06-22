openmediavault/openmediavault

commit 8496de967ecba61422b0956eb362a1428ec7b03f

Improve the requiredIf validator
The form field's validator configuration is updated automatically to let the UI reflect the status by adding or removing the `*` flag of the form field label.

Signed-off-by: Volker Theile <votdev@gmx.de>

deb/openmediavault/workbench/src/app/shared/forms/custom-validators.ts
@@ -107,17 +107,19 @@ const getFormValues = (control: AbstractControl): Array<any> => {
 export class CustomValidators {
   /**
    * Validator that requires the control have a non-empty value if the
-   * specified constraint succeeds.
+   * specified constraint succeeds. The form field's validator configuration
+   * is updated automatically to let the UI reflect the status by adding or
+   * removing the `*` flag of the form field label.
    *
-   * @param constraint The constraint to process.
+   * @param validators The form field validators.
    * @returns A validator function that returns an error map with the
    *   `required` property if the validation constraint succeeds and
    *   the control's value is empty, otherwise `null`.
    */
-  static requiredIf(constraint: Constraint): ValidatorFn {
+  static requiredIf(validators: Record<string, any>): ValidatorFn {
     let hasSubscribed = false;
     // Determine the properties involved in the constraint.
-    const props = ConstraintService.getProps(constraint);
+    const props = ConstraintService.getProps(validators.requiredIf);
     return (control: AbstractControl): ValidationErrors | null => {
       if (!control.parent) {
         return null;
@@ -136,11 +138,13 @@ export class CustomValidators {
         });
         hasSubscribed = true;
       }
-      const fulfilled = ConstraintService.test(constraint, getFormValues(control));
-      if (!fulfilled) {
-        return null;
-      }
-      return isEmptyInputValue(control.value) ? { required: true } : null;
+      const fulfilled = ConstraintService.test(validators.requiredIf, getFormValues(control));
+      // Update the form field's `required` validator configuration to let
+      // the UI reflect the status by adding or removing the `*` flag of the
+      // form field label.
+      validators.required = fulfilled as boolean;
+      // Return the validator response as usual.
+      return !fulfilled && isEmptyInputValue(control.value) ? { required: true } : null;
     };
   }
 

deb/openmediavault/workbench/src/app/core/components/intuition/form/form.component.ts
@@ -272,7 +272,7 @@ export class FormComponent implements AfterViewInit, OnInit {
           validators.push(Validators.email);
         }
         if (_.isPlainObject(field.validators.requiredIf)) {
-          validators.push(CustomValidators.requiredIf(field.validators.requiredIf));
+          validators.push(CustomValidators.requiredIf(field.validators));
         }
         if (_.isArray(field.validators.custom)) {
           _.forEach(field.validators.custom, (custom: FormFieldConstraintValidator) => {

commit 7887f478efdf6d8fd33e4ad3b52cc71680c7c7a3

Fix bug introduced with 8496de967ecba61422b0956eb362a1428ec7b03f
Signed-off-by: Volker Theile <votdev@gmx.de>

deb/openmediavault/workbench/src/app/shared/forms/custom-validators.ts
@@ -138,13 +138,15 @@ export class CustomValidators {
         });
         hasSubscribed = true;
       }
-      const fulfilled = ConstraintService.test(validators.requiredIf, getFormValues(control));
+      const fulfilled: any = ConstraintService.test(validators.requiredIf, getFormValues(control));
       // Update the form field's `required` validator configuration to let
       // the UI reflect the status by adding or removing the `*` flag of the
       // form field label.
-      validators.required = fulfilled as boolean;
+      if (_.isBoolean(fulfilled)) {
+        validators.required = fulfilled;
+      }
       // Return the validator response as usual.
-      return !fulfilled && isEmptyInputValue(control.value) ? { required: true } : null;
+      return fulfilled && isEmptyInputValue(control.value) ? { required: true } : null;
     };
   }
 