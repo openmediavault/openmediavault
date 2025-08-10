c78557d2708485cc807681721f7ace67cbd91dfc
Volker Theile <votdev@gmx.de>
2025-08-10 12:30:59 +0200

    openmediavault 8 / Debian 13
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- pbuilder/README.md
+++ pbuilder/README.md
@@ -1,1 +1,17 @@
-Make sure qemu >= 5.0 is installed.
+These helper scripts for pbuilder are used to construct a chroot system.
+This chroot environments can then be used to build custom packages for openmediavault.
+
+To install all dependencies, run the following command:
+```shell
+$ make install_deps
+```
+
+To create a new chroot system for a given platform, run the following command:
+```shell
+$ ./create.sh arm64
+```
+
+To build a package, run:
+```shell
+$ ./build.sh arm64 <PACKAGENAME>.dsc
+```
--- vagrant/debian/install.sh
+++ vagrant/debian/install.sh
@@ -39,6 +39,11 @@ export LANG=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
 
+# Replace the cloud-optimized kernel with the Debian standard kernel.
+apt-get install --yes linux-image-amd64
+apt-get remove --yes --purge linux-image-cloud-amd64
+apt-get autoremove --yes
+
 # Install the openmediavault keyring manually.
 apt-get install --yes gnupg
 wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | \
@@ -46,31 +51,31 @@ wget --quiet --output-document=- https://packages.openmediavault.org/public/arch
 
 # Install openmediavault.
 cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
-deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm main
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm main
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public synchrony main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages synchrony main
 ## Uncomment the following line to add software from the proposed repository.
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm-proposed main
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm-proposed main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public synchrony-proposed main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages synchrony-proposed main
 ## This software is not part of OpenMediaVault, but is offered by third-party
 ## developers as a service to OpenMediaVault users.
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm partner
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm partner
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public synchrony partner
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages synchrony partner
 EOF
 apt-get update
-apt-get --yes --auto-remove --show-upgraded \
-	--allow-downgrades --allow-change-held-packages \
-	--no-install-recommends \
-	--option DPkg::Options::="--force-confdef" \
-	--option DPkg::Options::="--force-confold" \
-	install openmediavault
+# apt-get --yes --auto-remove --show-upgraded \
+# 	--allow-downgrades --allow-change-held-packages \
+# 	--no-install-recommends \
+# 	--option DPkg::Options::="--force-confdef" \
+# 	--option DPkg::Options::="--force-confold" \
+# 	install openmediavault
 
 # Populate the database.
-omv-confdbadm populate
+# omv-confdbadm populate
 
 # Deploy the /etc/hosts file to ensure the hostname can be resolved
 # properly for IPv4 and IPv6. Otherwise building the Salt grains
 # (core.fqdns and core.ip_fqdn) will take a very long time.
-omv-salt deploy run hosts
+# omv-salt deploy run hosts
 
 # Display the login information.
 cat /etc/issue
