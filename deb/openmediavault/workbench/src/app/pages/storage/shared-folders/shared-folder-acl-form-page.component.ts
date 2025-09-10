openmediavault/openmediavault

commit b4474cd8c1e53b4571ec91e3bcbe561a7e3c9713

Improve loading implementation in pages (#1987)
Signed-off-by: Volker Theile <votdev@gmx.de>

deb/openmediavault/workbench/src/app/pages/storage/shared-folders/shared-folder-acl-form-page.component.ts
@@ -19,8 +19,8 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 import { marker as gettext } from '@ngneat/transloco-keys-manager/marker';
 import * as _ from 'lodash';
-import { EMPTY } from 'rxjs';
-import { catchError, distinctUntilChanged, finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { distinctUntilChanged, finalize } from 'rxjs/operators';
 
 import { FormValues } from '~/app/core/components/intuition/models/form.type';
 import {
@@ -309,10 +309,15 @@ export class SharedFolderAclFormPageComponent extends BaseFormPageComponent impl
 
   ngOnInit(): void {
     const self = this.page;
-    self.loadData = () => this.loadData('/');
+    // @ts-ignore
+    self.doLoadData = (): Observable<RpcObjectResponse> => this.doLoadData('/');
+    // @ts-ignore
+    self.onLoadData = (res: RpcObjectResponse): void => this.onLoadData(res);
     self.afterViewInitEvent.subscribe(() => {
       const control: AbstractControl = self.form.formGroup.get('file');
-      control?.valueChanges.pipe(distinctUntilChanged()).subscribe((value) => this.loadData(value));
+      control?.valueChanges.pipe(distinctUntilChanged()).subscribe((value) => {
+        this.doLoadData(value).subscribe();
+      });
     });
   }
 
@@ -385,44 +390,34 @@ export class SharedFolderAclFormPageComponent extends BaseFormPageComponent impl
       });
   }
 
-  protected loadData(file: string) {
+  protected doLoadData(file: string): Observable<RpcObjectResponse> {
     const uuid: string = _.get(this.page.pageContext._routeParams, 'uuid');
-    this.page.pageContextService.startLoading();
-    this.rpcService
-      .request('ShareMgmt', 'getFileACL', {
-        uuid,
-        file
+    return this.rpcService.request('ShareMgmt', 'getFileACL', {
+      uuid,
+      file
+    });
+  }
+
+  protected onLoadData(res: RpcObjectResponse): void {
+    _.map(res.acl.users, (user: Record<string, any>) =>
+      _.merge(user, {
+        id: _.get(user, 'uid'),
+        type: 'user'
       })
-      .pipe(
-        catchError((error) => {
-          this.page.pageContextService.setError(error);
-          return EMPTY;
-        }),
-        finalize(() => {
-          this.page.pageContextService.stopLoading();
-        })
-      )
-      .subscribe((res: RpcObjectResponse) => {
-        _.map(res.acl.users, (user: Record<string, any>) =>
-          _.merge(user, {
-            id: _.get(user, 'uid'),
-            type: 'user'
-          })
-        );
-        _.map(res.acl.groups, (group: Record<string, any>) =>
-          _.merge(group, {
-            id: _.get(group, 'gid'),
-            type: 'group'
-          })
-        );
-        this.page.setFormValues({
-          owner: res.owner,
-          group: res.group,
-          userperms: res.acl.user,
-          groupperms: res.acl.group,
-          otherperms: res.acl.other,
-          perms: _.concat(res.acl.users, res.acl.groups)
-        });
-      });
+    );
+    _.map(res.acl.groups, (group: Record<string, any>) =>
+      _.merge(group, {
+        id: _.get(group, 'gid'),
+        type: 'group'
+      })
+    );
+    this.page.setFormValues({
+      owner: res.owner,
+      group: res.group,
+      userperms: res.acl.user,
+      groupperms: res.acl.group,
+      otherperms: res.acl.other,
+      perms: _.concat(res.acl.users, res.acl.groups)
+    });
   }
 }

deb/openmediavault/workbench/src/app/core/components/intuition/abstract-page-component.ts
@@ -17,11 +17,14 @@
  */
 import { AfterViewInit, Directive, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import * as _ from 'lodash';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 
 import { PageHintConfig } from '~/app/core/components/intuition/models/page-config.type';
 import { PageContext } from '~/app/core/models/page-context.type';
 import { PageContextService } from '~/app/core/services/page-context.service';
 import { formatDeep, isFormatable } from '~/app/functions.helper';
+import { RpcObjectResponse } from '~/app/shared/models/rpc.model';
 
 @Directive()
 // eslint-disable-next-line @angular-eslint/directive-class-suffix
@@ -57,6 +60,19 @@ export abstract class AbstractPageComponent<T> implements AfterViewInit, OnInit 
 
   ngAfterViewInit(): void {
     this.afterViewInitEvent.emit();
+  }
+
+  loadData(...args: any): Observable<RpcObjectResponse> {
+    this.pageContextService.startLoading();
+    return this.doLoadData(...args).pipe(
+      catchError((error: any) => {
+        this.pageContextService.setError(error);
+        return EMPTY;
+      }),
+      finalize(() => {
+        this.pageContextService.stopLoading();
+      })
+    );
   }
 
   /**
@@ -96,4 +112,19 @@ export abstract class AbstractPageComponent<T> implements AfterViewInit, OnInit 
       }
     });
   }
+
+  /**
+   * Load the data for this page. Override this method to load the data.
+   * @returns An observable that emits the loaded data.
+   */
+  protected doLoadData(...args: any): Observable<any> {
+    return EMPTY;
+  }
+
+  /**
+   * Use this function body as a template for processing the data loaded using
+   * `doLoadData`. Override this method in your code.
+   * @param res The response data.
+   */
+  protected onLoadData(res: any): void {}
 }

deb/openmediavault/workbench/src/app/core/components/intuition/code-editor-page/code-editor-page.component.html
@@ -12,7 +12,7 @@
     <button *ngIf="config.hasReloadButton && !config.autoReload"
             mat-icon-button
             matTooltip="{{ 'Reload' | transloco }}"
-            (click)="loadData()">
+            (click)="onReload()">
       <mat-icon svgIcon="{{ icon.reload }}"></mat-icon>
     </button>
   </mat-toolbar>

deb/openmediavault/workbench/src/app/core/components/intuition/code-editor-page/code-editor-page.component.ts
@@ -50,8 +50,7 @@ import {
 } from '@codemirror/view';
 import { marker as gettext } from '@ngneat/transloco-keys-manager/marker';
 import * as _ from 'lodash';
-import { EMPTY, Subscription, timer } from 'rxjs';
-import { catchError, finalize } from 'rxjs/operators';
+import { exhaustMap, Observable, Subscription, throwError, timer } from 'rxjs';
 
 import { AbstractPageComponent } from '~/app/core/components/intuition/abstract-page-component';
 import {
@@ -107,13 +106,14 @@ export class CodeEditorPageComponent
 
   override ngOnInit(): void {
     super.ngOnInit();
+    const intervalDuration =
+      _.isNumber(this.config.autoReload) && this.config.autoReload > 0
+        ? this.config.autoReload
+        : null;
     this.subscriptions.add(
-      timer(
-        0,
-        _.isNumber(this.config.autoReload) ? (this.config.autoReload as number) : null
-      ).subscribe(() => {
-        this.loadData();
-      })
+      timer(0, intervalDuration)
+        .pipe(exhaustMap(() => this.loadData()))
+        .subscribe((res: any) => this.onLoadData(res))
     );
     this.subscriptions.add(
       this.prefersColorSchemeService.change$.subscribe(
@@ -149,39 +149,33 @@ export class CodeEditorPageComponent
     }
   }
 
-  loadData(): void {
+  onReload() {
+    this.subscriptions.add(this.loadData().subscribe((res: any) => this.onLoadData(res)));
+  }
+
+  protected override doLoadData(): Observable<any> {
     const request = this.config.request;
-    if (_.isPlainObject(request) && _.isString(request.service) && _.isPlainObject(request.get)) {
-      if (this.pageContextService.isLoading()) {
-        return;
-      }
-      this.pageContextService.startLoading();
-      // noinspection DuplicatedCode
-      this.rpcService[request.get.task ? 'requestTask' : 'request'](
-        request.service,
-        request.get.method,
-        request.get.params
-      )
-        .pipe(
-          catchError((error) => {
-            this.pageContextService.setError(error);
-            return EMPTY;
-          }),
-          finalize(() => {
-            this.pageContextService.stopLoading();
-          })
-        )
-        .subscribe((res: any) => {
-          if (_.isString(request.get.format) && RpcObjectResponse.isType(res)) {
-            res = RpcObjectResponse.format(request.get.format, res);
-          }
-          const state: EditorState = this._editorView.state;
-          const transaction = state.update({
-            changes: { from: 0, to: state.doc.length, insert: res }
-          });
-          this._editorView.dispatch(transaction);
-        });
+    if (!(_.isString(request?.service) && _.isPlainObject(request?.get))) {
+      return throwError(() => new Error('Invalid request configuration.'));
     }
+    return this.rpcService[request.get.task ? 'requestTask' : 'request'](
+      request.service,
+      request.get.method,
+      request.get.params
+    );
+  }
+
+  protected override onLoadData(res: any): void {
+    const request = this.config.request;
+    const state: EditorState = this._editorView.state;
+    let text: string = res as string;
+    if (_.isString(request.get.format) && RpcObjectResponse.isType(res)) {
+      text = RpcObjectResponse.format(request.get.format, res);
+    }
+    const transaction = state.update({
+      changes: { from: 0, to: state.doc.length, insert: text }
+    });
+    this._editorView.dispatch(transaction);
   }
 
   protected override sanitizeConfig() {

deb/openmediavault/workbench/src/app/core/components/intuition/datatable-page/datatable-page.component.html
@@ -36,7 +36,7 @@
                    [selectionType]="config.selectionType"
                    (selectionChangeEvent)="onSelectionChange($event)"
                    [updateSelectionOnReload]="config.updateSelectionOnReload"
-                   (loadDataEvent)="loadData($event)"
+                   (loadDataEvent)="onLoadDataEvent($event)"
                    [remoteSorting]="config.remoteSorting"
                    [remotePaging]="config.remotePaging"
                    [remoteSearching]="config.remoteSearching"

deb/openmediavault/workbench/src/app/core/components/intuition/datatable-page/datatable-page.component.ts
@@ -19,7 +19,7 @@ import { Component, Inject, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { marker as gettext } from '@ngneat/transloco-keys-manager/marker';
 import * as _ from 'lodash';
-import { concat } from 'rxjs';
+import { concat, Observable, Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 
 import { AbstractPageComponent } from '~/app/core/components/intuition/abstract-page-component';
@@ -29,6 +29,7 @@ import { DatatablePageConfig } from '~/app/core/components/intuition/models/data
 import { DatatablePageButtonConfig } from '~/app/core/components/intuition/models/datatable-page-config.type';
 import { FormFieldConfig } from '~/app/core/components/intuition/models/form-field-config.type';
 import { PageContextService } from '~/app/core/services/page-context.service';
+import { Unsubscribe } from '~/app/decorators';
 import { format, formatDeep, isFormatable } from '~/app/functions.helper';
 import { translate } from '~/app/i18n.helper';
 import {
@@ -41,10 +42,9 @@ import { Icon } from '~/app/shared/enum/icon.enum';
 import { NotificationType } from '~/app/shared/enum/notification-type.enum';
 import { DatatableAction } from '~/app/shared/models/datatable-action.type';
 import { DatatableSelection } from '~/app/shared/models/datatable-selection.model';
-import { RpcListResponse } from '~/app/shared/models/rpc.model';
 import { BlockUiService } from '~/app/shared/services/block-ui.service';
 import { ClipboardService } from '~/app/shared/services/clipboard.service';
-import { DataStoreService } from '~/app/shared/services/data-store.service';
+import { DataStoreResponse, DataStoreService } from '~/app/shared/services/data-store.service';
 import { DialogService } from '~/app/shared/services/dialog.service';
 import { NotificationService } from '~/app/shared/services/notification.service';
 import { RpcService } from '~/app/shared/services/rpc.service';
@@ -58,6 +58,9 @@ import { RpcService } from '~/app/shared/services/rpc.service';
 export class DatatablePageComponent extends AbstractPageComponent<DatatablePageConfig> {
   @ViewChild('table', { static: true })
   table: DatatableComponent;
+
+  @Unsubscribe()
+  private subscriptions = new Subscription();
 
   protected count = 0;
   protected selection = new DatatableSelection();
@@ -78,55 +81,22 @@ export class DatatablePageComponent extends AbstractPageComponent<DatatablePageC
     });
   }
 
-  loadData(params: DataTableLoadParams) {
-    const store = this.config.store;
-    if (_.isPlainObject(store.proxy)) {
-      _.defaultsDeep(store.proxy.get, {
-        params: {
-          start: 0,
-          limit: -1
-        }
-      });
-      // Convert paging and sorting parameters.
-      if (_.isNumber(params.offset) && _.isNumber(params.limit)) {
-        _.merge(store.proxy.get.params, {
-          start: params.offset * params.limit,
-          limit: params.limit
-        });
-      }
-      if (_.isString(params.dir) && _.isString(params.prop)) {
-        _.merge(store.proxy.get.params, {
-          sortdir: params.dir,
-          sortfield: params.prop
-        });
-      }
-      if (!_.isUndefined(params.search)) {
-        _.merge(store.proxy.get.params, {
-          search: params.search
-        });
-      }
-    }
-    this.pageContextService.startLoading();
-    this.dataStoreService
-      .load(store)
-      .pipe(
-        finalize(() => {
-          this.pageContextService.stopLoading();
-        })
-      )
-      .subscribe(
-        (res: RpcListResponse) => {
+  onLoadDataEvent(params: DataTableLoadParams) {
+    this.subscriptions.add(
+      this.loadData(params).subscribe(
+        (res: DataStoreResponse) => {
           // Update the total count of all rows.
-          if (_.isPlainObject(store.proxy)) {
+          if (_.isPlainObject(this.config.store.proxy)) {
             this.count = res.total;
           }
         },
         () => {
           // Reset store and table in case of an error.
-          store.data = [];
+          this.config.store.data = [];
           this.count = 0;
         }
-      );
+      )
+    );
   }
 
   /**
@@ -408,6 +378,37 @@ export class DatatablePageComponent extends AbstractPageComponent<DatatablePageC
       'store.proxy.post.params',
       'store.filters'
     ]);
+  }
+
+  protected override doLoadData(params: DataTableLoadParams): Observable<DataStoreResponse> {
+    const store = this.config.store;
+    if (_.isPlainObject(store.proxy)) {
+      _.defaultsDeep(store.proxy.get, {
+        params: {
+          start: 0,
+          limit: -1
+        }
+      });
+      // Convert paging and sorting parameters.
+      if (_.isNumber(params.offset) && _.isNumber(params.limit)) {
+        _.merge(store.proxy.get.params, {
+          start: params.offset * params.limit,
+          limit: params.limit
+        });
+      }
+      if (_.isString(params.dir) && _.isString(params.prop)) {
+        _.merge(store.proxy.get.params, {
+          sortdir: params.dir,
+          sortfield: params.prop
+        });
+      }
+      if (!_.isUndefined(params.search)) {
+        _.merge(store.proxy.get.params, {
+          search: params.search
+        });
+      }
+    }
+    return this.dataStoreService.load(store);
   }
 
   private sanitizeActionsConfig(actions: DatatablePageActionConfig[]) {

deb/openmediavault/workbench/src/app/core/components/intuition/form-page/form-page.component.ts
@@ -19,8 +19,8 @@ import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/co
 import { ActivatedRoute, Router } from '@angular/router';
 import { marker as gettext } from '@ngneat/transloco-keys-manager/marker';
 import * as _ from 'lodash';
-import { EMPTY, Subscription, timer } from 'rxjs';
-import { catchError, debounceTime, finalize } from 'rxjs/operators';
+import { EMPTY, exhaustMap, Observable, Subscription, throwError, timer } from 'rxjs';
+import { debounceTime, finalize } from 'rxjs/operators';
 
 import { AbstractPageComponent } from '~/app/core/components/intuition/abstract-page-component';
 import { FormComponent } from '~/app/core/components/intuition/form/form.component';
@@ -149,55 +149,6 @@ export class FormPageComponent
 
   markAsPristine(): void {
     this.form.formGroup.markAsPristine();
-  }
-
-  loadData(): void {
-    const request = this.config.request;
-    if (_.isString(request?.service) && _.isPlainObject(request?.get)) {
-      if (this.pageContextService.isLoading()) {
-        return;
-      }
-      if (_.isString(request.get.onlyIf)) {
-        const result: string = format(request.get.onlyIf, this.pageContext);
-        if (false === toBoolean(result)) {
-          return;
-        }
-      }
-      this.pageContextService.startLoading();
-      // noinspection DuplicatedCode
-      this.rpcService[request.get.task ? 'requestTask' : 'request'](
-        request.service,
-        request.get.method,
-        request.get.params
-      )
-        .pipe(
-          catchError((error) => {
-            this.pageContextService.setError(error);
-            return EMPTY;
-          }),
-          finalize(() => {
-            this.pageContextService.stopLoading();
-          })
-        )
-        .subscribe((res: RpcObjectResponse) => {
-          this.onLoadData(res);
-        });
-    }
-  }
-
-  onLoadData(res: RpcObjectResponse): void {
-    const request = this.config.request;
-    // Transform the request response?
-    if (_.isPlainObject(request?.get?.transform)) {
-      res = RpcObjectResponse.transform(res, request.get.transform);
-    }
-    // Filter the request response?
-    if (_.isPlainObject(request?.get?.filter)) {
-      const filterConfig = request.get.filter;
-      res = RpcObjectResponse.filter(res, filterConfig.props, filterConfig.mode);
-    }
-    // Update the form field values.
-    this.setFormValues(res);
   }
 
   /**
@@ -497,9 +448,9 @@ export class FormPageComponent
           ? this.config.autoReload
           : null;
       this.subscriptions.add(
-        timer(0, intervalDuration).subscribe(() => {
-          this.loadData();
-        })
+        timer(0, intervalDuration)
+          .pipe(exhaustMap(() => this.loadData()))
+          .subscribe((res: RpcObjectResponse) => this.onLoadData(res))
       );
     } else {
       // Inject the query parameters of the route into the form fields.
@@ -511,5 +462,38 @@ export class FormPageComponent
         }
       });
     }
+  }
+
+  protected override doLoadData(): Observable<RpcObjectResponse> {
+    const request = this.config.request;
+    if (!(_.isString(request?.service) && _.isPlainObject(request?.get))) {
+      return throwError(() => new Error('Invalid request configuration.'));
+    }
+    if (_.isString(request.get.onlyIf)) {
+      const result: string = format(request.get.onlyIf, this.pageContext);
+      if (false === toBoolean(result)) {
+        return EMPTY;
+      }
+    }
+    return this.rpcService[request.get.task ? 'requestTask' : 'request'](
+      request.service,
+      request.get.method,
+      request.get.params
+    );
+  }
+
+  protected override onLoadData(res: RpcObjectResponse): void {
+    const request = this.config.request;
+    // Transform the request response?
+    if (_.isPlainObject(request?.get?.transform)) {
+      res = RpcObjectResponse.transform(res, request.get.transform);
+    }
+    // Filter the request response?
+    if (_.isPlainObject(request?.get?.filter)) {
+      const filterConfig = request.get.filter;
+      res = RpcObjectResponse.filter(res, filterConfig.props, filterConfig.mode);
+    }
+    // Update the form field values.
+    this.setFormValues(res);
   }
 }

deb/openmediavault/workbench/src/app/core/components/intuition/rrd-page/rrd-page.component.ts
@@ -18,8 +18,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { marker as gettext } from '@ngneat/transloco-keys-manager/marker';
 import * as _ from 'lodash';
-import { EMPTY, Subscription } from 'rxjs';
-import { catchError, finalize } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 import { AbstractPageComponent } from '~/app/core/components/intuition/abstract-page-component';
 import {
@@ -31,7 +31,7 @@ import { Unsubscribe } from '~/app/decorators';
 import { format, formatDeep, unixTimeStamp } from '~/app/functions.helper';
 import { Icon } from '~/app/shared/enum/icon.enum';
 import { BlockUiService } from '~/app/shared/services/block-ui.service';
-import { DataStoreService } from '~/app/shared/services/data-store.service';
+import { DataStoreResponse, DataStoreService } from '~/app/shared/services/data-store.service';
 import { RpcService } from '~/app/shared/services/rpc.service';
 
 @Component({
@@ -79,19 +79,8 @@ export class RrdPageComponent extends AbstractPageComponent<RrdPageConfig> imple
       })
     );
     if (this.config?.store) {
-      this.pageContextService.startLoading();
-      this.dataStoreService
-        .load(this.config.store)
-        .pipe(
-          catchError((error) => {
-            this.pageContextService.setError(error);
-            return EMPTY;
-          }),
-          finalize(() => {
-            this.pageContextService.stopLoading();
-          })
-        )
-        .subscribe(() => {
+      this.subscriptions.add(
+        this.loadData().subscribe(() => {
           _.forEach(this.config.store.data, (item: Record<any, any>) => {
             const label = format(this.config.label, item);
             const graphs: RrdPageGraphConfig[] = _.map(
@@ -100,7 +89,8 @@ export class RrdPageComponent extends AbstractPageComponent<RrdPageConfig> imple
             );
             this.tabs.push({ label, graphs });
           });
-        });
+        })
+      );
     }
   }
 
@@ -117,5 +107,9 @@ export class RrdPageComponent extends AbstractPageComponent<RrdPageConfig> imple
         // Force redrawing the images.
         this.time = unixTimeStamp();
       });
+  }
+
+  protected override doLoadData(): Observable<DataStoreResponse> {
+    return this.dataStoreService.load(this.config.store);
   }
 }

commit c5de3d5fdfd286f351ae16191eb2f06ecde9b593

Issue #2013: ACLs aren't loaded correctly when changing the relative path
This is a regression introduced by https://github.com/openmediavault/openmediavault/pull/1987.

Fixes: https://github.com/openmediavault/openmediavault/issues/2013

Signed-off-by: Volker Theile <votdev@gmx.de>

deb/openmediavault/workbench/src/app/pages/storage/shared-folders/shared-folder-acl-form-page.component.ts
@@ -321,7 +321,7 @@ export class SharedFolderAclFormPageComponent extends BaseFormPageComponent impl
       self.afterViewInitEvent.subscribe(() => {
         const control: AbstractControl = self.form.formGroup.get('file');
         control?.valueChanges.pipe(distinctUntilChanged()).subscribe((value) => {
-          this.doLoadData(value).subscribe();
+          this.doLoadData(value).subscribe((res: RpcObjectResponse) => this.onLoadData(res));
         });
       })
     );