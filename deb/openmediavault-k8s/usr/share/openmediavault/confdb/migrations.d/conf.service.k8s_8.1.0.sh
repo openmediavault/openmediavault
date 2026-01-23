68b4366c8dffdcbe147efba021f253d14d12bb9b
Volker Theile <votdev@gmx.de>
2026-01-23 09:03:21 +0100

    Add UI support to configure additional Traefik ports
    The following environment variables have been removed:
    - OMV_K8S_TRAEFIK_PORTS
    - OMV_K8S_TRAEFIK_ENTRYPOINT_TRANSPORT_RESPONDINGTIMEOUTS_READTIMEOUT
    
    Related to: https://github.com/openmediavault/openmediavault/issues/1940
    
    Signed-off-by: Volker Theile <votdev@gmx.de>

---
+++ deb/openmediavault-k8s/usr/share/openmediavault/confdb/migrations.d/conf.service.k8s_8.1.0.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# This file is part of OpenMediaVault.
+#
+# @license   https://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @copyright Copyright (c) 2009-2026 Volker Theile
+#
+# OpenMediaVault is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# OpenMediaVault is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OpenMediaVault. If not, see <https://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+omv_config_add_node "/config/services/k8s" "lbports"
+
+if ! omv-confdbadm exists --filter '{"operator":"stringEquals","arg0":"name","arg1":"web"}' \
+		"conf.service.k8s.lbport"; then
+	webport=$(omv_config_get "/config/services/k8s/webport")
+	omv-confdbadm read --defaults "conf.service.k8s.lbport" | \
+		jq ".name = \"web\" | .port = ${webport} | .exposedport = ${webport} | .protocol = \"tcp\" | .expose = true | .comment = \"HTTP\" | .extravalues=\"transport:\\n  respondingTimeouts:\\n    readTimeout: 60\"" | \
+		omv-confdbadm update "conf.service.k8s.lbport" -
+fi
+if ! omv-confdbadm exists --filter '{"operator":"stringEquals","arg0":"name","arg1":"websecure"}' \
+		"conf.service.k8s.lbport"; then
+	websecureport=$(omv_config_get "/config/services/k8s/websecureport")
+	omv-confdbadm read --defaults "conf.service.k8s.lbport" | \
+		jq ".name = \"websecure\" | .port = ${websecureport} | .exposedport = ${websecureport} | .protocol = \"tcp\" | .expose = true | .comment = \"HTTPS\" | .extravalues=\"transport:\\n  respondingTimeouts:\\n    readTimeout: 60\"" | \
+		omv-confdbadm update "conf.service.k8s.lbport" -
+fi
+if ! omv-confdbadm exists --filter '{"operator":"stringEquals","arg0":"name","arg1":"dashboard"}' \
+		"conf.service.k8s.lbport"; then
+	dashboardport=$(omv_config_get "/config/services/k8s/dashboardport")
+	omv-confdbadm read --defaults "conf.service.k8s.lbport" | \
+		jq ".name = \"dashboard\" | .port = ${dashboardport} | .exposedport = ${dashboardport} | .protocol = \"tcp\" | .expose = true | .comment = \"Dashboard\" | .extravalues= \"tls:\\n  enabled: true\"" | \
+		omv-confdbadm update "conf.service.k8s.lbport" -
+fi
+
+omv_config_delete "/config/services/k8s/webport"
+omv_config_delete "/config/services/k8s/websecureport"
+omv_config_delete "/config/services/k8s/dashboardport"
+
+exit 0
