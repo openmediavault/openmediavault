b7703789aae2701e1f1b6a3b252511ce86ab0d62
Volker Theile <votdev@gmx.de>
2026-02-07 13:58:49 +0100

    Improve install script
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- install.sh
+++ install.sh
@@ -22,48 +22,74 @@
 set -e
 set -o noglob
 
-. /etc/os-release
-
 OMV_VERSION="8"
 OMV_VERSION_CODENAME="synchrony"
+OMV_GIT_URL="https://github.com/openmediavault/openmediavault/raw/master/deb/openmediavault/"
 DEBIAN_VERSION_CODENAME="trixie"
 
-if [ "$(id -u)" -ne 0 ]; then
-	echo "The installation script must be executed as user 'root'." >&2
-	exit 77
-fi
+info() {
+	echo "[INFO] $@"
+}
 
-if [ "${VERSION_CODENAME}" != "${DEBIAN_VERSION_CODENAME}" ]; then
-	echo "The Debian version must be '${DEBIAN_VERSION_CODENAME}', found '${VERSION_CODENAME}'." >&2
-	exit 1
-fi
+warn() {
+	echo "[WARN] $@" >&2
+}
 
-arch="$(dpkg --print-architecture)"
-case "${arch}" in
-	amd64|arm64)
-		;;
-	*)
-		echo "The architecture '${arch}' is not supported." >&2
+error() {
+	echo "[ERROR] $@" >&2
+}
+
+install_verify_user() {
+	if [ "$(id -u)" -ne 0 ]; then
+		error "The installation script must be executed as user 'root'."
+		exit 77
+	fi
+}
+
+install_verify_codename() {
+	. /etc/os-release
+	if [ "${VERSION_CODENAME}" != "${DEBIAN_VERSION_CODENAME}" ]; then
+		error "The Debian version must be '${DEBIAN_VERSION_CODENAME}', found '${VERSION_CODENAME}'."
 		exit 1
-		;;
-esac
+	fi
+}
 
-if dpkg --list | grep --quiet --extended-regexp --word-regexp "gdm3|lightdm|lxdm|sddm|slim|wdm|xdm"; then
-  echo "The system is running a desktop environment! Please check https://docs.openmediavault.org/en/${OMV_VERSION}.x/installation/on_debian.html for more details." >&2
-  exit 1
-fi
+install_verify_arch() {
+	arch="$(dpkg --print-architecture)"
+	case "${arch}" in
+		amd64|arm64)
+			;;
+		*)
+			error "The architecture '${arch}' is not supported."
+			exit 1
+			;;
+	esac
+}
 
-echo "Installing openmediavault (${OMV_VERSION_CODENAME}) ..."
+install_verify_de() {
+	if dpkg --list | grep --quiet --extended-regexp --word-regexp "gdm3|lightdm|lxdm|sddm|slim|wdm|xdm"; then
+		error "The system is running a desktop environment! Please check https://docs.openmediavault.org/en/${OMV_VERSION}.x/installation/on_debian.html for more details."
+		exit 1
+	fi
+}
 
-export LANG=C.UTF-8
-export DEBIAN_FRONTEND=noninteractive
-export APT_LISTCHANGES_FRONTEND=none
+do_install() {
+	info "Installing openmediavault (${OMV_VERSION_CODENAME}) ..."
 
-apt-get install --yes gnupg
-wget --output-document=- https://packages.openmediavault.org/public/archive.key | \
-	gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
+	install_verify_user
+	install_verify_codename
+	install_verify_arch
+	install_verify_de
 
-cat <<EOF > /etc/apt/sources.list.d/openmediavault.list
+	export LANG=C.UTF-8
+	export DEBIAN_FRONTEND=noninteractive
+	export APT_LISTCHANGES_FRONTEND=none
+
+	apt-get install --yes gnupg
+	wget --output-document=- https://packages.openmediavault.org/public/archive.key | \
+		gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
+
+	cat <<EOF > /etc/apt/sources.list.d/openmediavault.list
 deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public ${OMV_VERSION_CODENAME} main
 # deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages ${OMV_VERSION_CODENAME} main
 ## Uncomment the following line to add software from the proposed repository.
@@ -75,23 +101,58 @@ deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://pa
 # deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages ${OMV_VERSION_CODENAME} partner
 EOF
 
-apt-get update
-apt-get --yes --auto-remove --show-upgraded \
-	--allow-downgrades --allow-change-held-packages \
-	--no-install-recommends \
-	--option DPkg::Options::="--force-confdef" \
-	--option DPkg::Options::="--force-confold" \
-	install openmediavault
+	apt-get update
+	apt-get --yes --auto-remove --show-upgraded \
+		--allow-downgrades --allow-change-held-packages \
+		--no-install-recommends \
+		--option DPkg::Options::="--force-confdef" \
+		--option DPkg::Options::="--force-confold" \
+		install openmediavault
 
-# Populate the database.
-omv-confdbadm populate
+	# Populate the database.
+	omv-confdbadm populate
 
-# Deploy the /etc/hosts file to ensure the hostname can be resolved
-# properly for IPv4 and IPv6. Otherwise building the Salt grains
-# (core.fqdns and core.ip_fqdn) will take a very long time.
-omv-salt deploy run hosts
+	# Deploy the /etc/hosts file to ensure the hostname can be resolved
+	# properly for IPv4 and IPv6. Otherwise building the Salt grains
+	# (core.fqdns and core.ip_fqdn) will take a very long time.
+	omv-salt deploy run hosts
 
-# Display the login information.
-cat /etc/issue
+	# Display the login information.
+	cat /etc/issue
+}
+
+do_fix() {
+	info "Fixing the openmediavault (${OMV_VERSION_CODENAME}) installation ..."
+
+	install_verify_user
+
+	# Fix dpkg-divert problems.
+	file="/usr/sbin/omv-mkaptidx"
+	wget --output-document="${file}" "${OMV_GIT_URL}/${file}"
+	chmod 755 "${file}"
+}
+
+command="install"
+while [ $# -gt 0 ]; do
+	case "$1" in
+	--fix)
+		command="fix"
+		shift
+		;;
+	*)
+		error "Unknown argument: $1"
+		exit 1
+		;;
+	esac
+done
+
+case "${command}" in
+	fix)
+		do_fix
+		;;
+	install)
+		do_install
+		;;
+esac
 
 exit 0
