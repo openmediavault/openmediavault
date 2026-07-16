openmediavault/openmediavault

commit 0f35cc37ad04d4ffb6d2269cd717366c63d30f6f

Improve username validation
Signed-off-by: Volker Theile <votdev@gmx.de>

deb/openmediavault/usr/lib/python3/dist-packages/openmediavault/datamodel/schema.py
@@ -145,7 +145,10 @@ class Schema(openmediavault.json.Schema):
                         name, "The value '%s' is not a valid share name." % value
                     ) from None
             elif "username" == schema['format']:
-                if not re.match(r'^[_.A-Za-z0-9][-\@_.A-Za-z0-9]*\$?$', value):
+                # Regular expression is taken from Debian tools.
+                # - https://github.com/shadow-maint/shadow/blob/55e75ec6b2f8878c6c269570a4470730092c1b39/lib/chkname.c#L52
+                # - https://salsa.debian.org/debian/adduser/-/blob/debian/latest/adduser.conf?ref_type=heads#L88
+                if not re.match(r'^[a-zA-Z0-9_.][a-zA-Z0-9_.-]*\$?$', value):
                     raise openmediavault.json.SchemaValidationException(
                         name, "The value '%s' is not a valid user name." % value
                     )

deb/openmediavault/usr/share/php/openmediavault/datamodel/schema.inc
@@ -146,9 +146,11 @@ class Schema extends \OMV\Json\Schema
                     }
                     break;
                 case "username":
-                    // Regular expression taken from Debian adduser.
+                    // Regular expression is taken from Debian tools.
+                    // - https://github.com/shadow-maint/shadow/blob/55e75ec6b2f8878c6c269570a4470730092c1b39/lib/chkname.c#L52
+                    // - https://salsa.debian.org/debian/adduser/-/blob/debian/latest/adduser.conf?ref_type=heads#L88
                     if (!preg_match(
-                        '/^[_.A-Za-z0-9][-\@_.A-Za-z0-9]*\$?$/',
+                        '/^[a-zA-Z0-9_.][a-zA-Z0-9_.-]*\$?$/',
                         $value
                     )) {
                         throw new \OMV\Json\SchemaValidationException(

deb/openmediavault/workbench/src/app/shared/forms/custom-validators.ts
@@ -34,9 +34,10 @@ import { ConstraintService } from '~/app/shared/services/constraint.service';
 
 const regExp = {
   /* eslint-disable max-len */
-  // Taken from Debian adduser command.
-  userName: /^[_.A-Za-z0-9][-\@_.A-Za-z0-9]*\$?$/,
-  groupName: /^[a-zA-Z0-9\-\.]+$/,
+  // Regular expression is taken from Debian tools.
+  // - https://github.com/shadow-maint/shadow/blob/55e75ec6b2f8878c6c269570a4470730092c1b39/lib/chkname.c#L52
+  // - https://salsa.debian.org/debian/adduser/-/blob/debian/latest/adduser.conf?ref_type=heads#L88
+  userName: /^[a-zA-Z0-9_.][a-zA-Z0-9_.-]*\$?$/,
   // We are using the SMB/CIFS file/directory naming convention for this:
   // All characters are legal in the basename and extension except the
   // space character (0x20) and:
@@ -266,7 +267,7 @@ export class CustomValidators {
       case 'userName':
         return CustomValidators.pattern(regExp.userName, gettext('Invalid user name.'));
       case 'groupName':
-        return CustomValidators.pattern(regExp.groupName, gettext('Invalid group name.'));
+        return CustomValidators.pattern(regExp.userName, gettext('Invalid group name.'));
       case 'shareName':
         return CustomValidators.pattern(
           regExp.shareName,

deb/openmediavault/usr/share/openmediavault/unittests/php/test_openmediavault_datamodel_schema.php
@@ -555,6 +555,105 @@ class test_openmediavault_datamodel_schema extends \PHPUnit\Framework\TestCase
     /**
      * @doesNotPerformAssertions
      */
+    public function testCheckFormatUsername1()
+    {
+        $schema = $this->getSchema();
+        $schema->checkFormat(
+            "john",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCheckFormatUsername2()
+    {
+        $schema = $this->getSchema();
+        $schema->checkFormat(
+            "john.doe",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCheckFormatUsername3()
+    {
+        $schema = $this->getSchema();
+        $schema->checkFormat(
+            "_ssh",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCheckFormatUsername4()
+    {
+        $schema = $this->getSchema();
+        $schema->checkFormat(
+            "john123",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCheckFormatUsername5()
+    {
+        // Samba machine accounts end with '$'.
+        $schema = $this->getSchema();
+        $schema->checkFormat(
+            "john$",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    public function testCheckFormatUsernameFail1()
+    {
+        $schema = $this->getSchema();
+        $this->expectException(\OMV\Json\SchemaValidationException::class);
+        $schema->checkFormat(
+            "john doe",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    public function testCheckFormatUsernameFail2()
+    {
+        $schema = $this->getSchema();
+        $this->expectException(\OMV\Json\SchemaValidationException::class);
+        $schema->checkFormat(
+            "-john",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    public function testCheckFormatUsernameFail3()
+    {
+        $schema = $this->getSchema();
+        $this->expectException(\OMV\Json\SchemaValidationException::class);
+        $schema->checkFormat(
+            "john!",
+            [ "format" => "username" ],
+            "field1"
+        );
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
     public function testCheckFormatNoMultiLine1()
     {
         $schema = $this->getSchema();

deb/openmediavault/usr/share/openmediavault/unittests/python/test_openmediavault_datamodel_schema.py
@@ -403,6 +403,54 @@ class SchemaTestCase(unittest.TestCase):
             )
         )
 
+    def test_check_format_username_1(self):
+        schema = openmediavault.datamodel.Schema({})
+        schema._check_format("john", {"format": "username"}, "field1")
+
+    def test_check_format_username_2(self):
+        schema = openmediavault.datamodel.Schema({})
+        schema._check_format("john.doe", {"format": "username"}, "field1")
+
+    def test_check_format_username_3(self):
+        schema = openmediavault.datamodel.Schema({})
+        schema._check_format("_apt", {"format": "username"}, "field1")
+
+    def test_check_format_username_4(self):
+        schema = openmediavault.datamodel.Schema({})
+        schema._check_format("john123", {"format": "username"}, "field1")
+
+    def test_check_format_username_5(self):
+        # Samba machine accounts end with '$'.
+        schema = openmediavault.datamodel.Schema({})
+        schema._check_format("john$", {"format": "username"}, "field1")
+
+    def test_check_format_username_fail_1(self):
+        schema = openmediavault.datamodel.Schema({})
+        self.assertRaises(
+            openmediavault.json.SchemaValidationException,
+            lambda: schema._check_format(
+                "john doe", {"format": "username"}, "field1"
+            )
+        )
+
+    def test_check_format_username_fail_2(self):
+        schema = openmediavault.datamodel.Schema({})
+        self.assertRaises(
+            openmediavault.json.SchemaValidationException,
+            lambda: schema._check_format(
+                "-john", {"format": "username"}, "field1"
+            )
+        )
+
+    def test_check_format_username_fail_3(self):
+        schema = openmediavault.datamodel.Schema({})
+        self.assertRaises(
+            openmediavault.json.SchemaValidationException,
+            lambda: schema._check_format(
+                "john!", {"format": "username"}, "field1"
+            )
+        )
+
 
 if __name__ == "__main__":
     unittest.main()

deb/openmediavault/workbench/src/app/shared/forms/custom-validators.spec.ts
@@ -450,6 +450,92 @@ describe('CustomValidators', () => {
       });
     });
 
+    describe('userName', () => {
+      let validator: ValidatorFn;
+
+      beforeEach(() => {
+        validator = CustomValidators.patternType('userName');
+      });
+
+      it('should validate userName [1]', () => {
+        formControl.setValue('john');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate userName [2]', () => {
+        formControl.setValue('john.doe');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate userName [3]', () => {
+        formControl.setValue('_apt');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate userName [4]', () => {
+        formControl.setValue('john123');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate userName [5] (Samba machine account)', () => {
+        formControl.setValue('john$');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should not validate userName [1] (space)', () => {
+        formControl.setValue('john doe');
+        expect(validator(formControl)).toEqual({ pattern: 'Invalid user name.' });
+      });
+
+      it('should not validate userName [2] (leading hyphen)', () => {
+        formControl.setValue('-john');
+        expect(validator(formControl)).toEqual({ pattern: 'Invalid user name.' });
+      });
+
+      it('should not validate userName [3] (special char)', () => {
+        formControl.setValue('john!');
+        expect(validator(formControl)).toEqual({ pattern: 'Invalid user name.' });
+      });
+    });
+
+    describe('groupName', () => {
+      let validator: ValidatorFn;
+
+      beforeEach(() => {
+        validator = CustomValidators.patternType('groupName');
+      });
+
+      it('should validate groupName [1]', () => {
+        formControl.setValue('docker-users');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate groupName [2]', () => {
+        formControl.setValue('mygroup');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate groupName [3]', () => {
+        formControl.setValue('my.group');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate groupName [4]', () => {
+        formControl.setValue('_ssh');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should not validate groupName [1] (space)', () => {
+        formControl.setValue('docker users');
+        expect(validator(formControl)).toEqual({ pattern: 'Invalid group name.' });
+      });
+
+      it('should not validate groupName [2] (at sign)', () => {
+        formControl.setValue('@group');
+        expect(validator(formControl)).toEqual({ pattern: 'Invalid group name.' });
+      });
+    });
+
     describe('netbiosName', () => {
       let validator: ValidatorFn;
 

commit 2f77f413cc916c5ae45e96c39973436cd9b761e1

Improve sharedfolder name validation
- Add sharedfolder name validation
- Limit name to max 80 chars. This is the limitation of SMB shares where the shared folder name is used.
- Fix regression introduced by https://github.com/openmediavault/openmediavault/commit/0f35cc37ad04d4ffb6d2269cd717366c63d30f6f

Related to: https://forum.openmediavault.org/index.php?thread/59328-problem-with-german-umlauts/

Signed-off-by: Volker Theile <votdev@gmx.de>

deb/openmediavault/usr/lib/python3/dist-packages/openmediavault/datamodel/schema.py
@@ -137,8 +137,7 @@ class Schema(openmediavault.json.Schema):
                 # http://tools.ietf.org/html/draft-leach-cifs-v1-spec-01
                 # http://msdn.microsoft.com/en-us/library/aa365247%28VS.85%29.aspx
                 if not re.match(
-                    r'^[^.]([^"/\\\[\]:+|<>=;,*?. ]+)?([.]'
-                    r'[^"/\\\[\]:+|<>=;,*?. ]+)*$',
+                    r'^[^"/\\\[\]:+|<>=;,*?. ]+(?:\.[^"/\\\[\]:+|<>=;,*?. ]+)*$',
                     value,
                 ):
                     raise openmediavault.json.SchemaValidationException(

deb/openmediavault/usr/share/php/openmediavault/datamodel/schema.inc
@@ -136,8 +136,10 @@ class Schema extends \OMV\Json\Schema
                     // References:
                     // http://tools.ietf.org/html/draft-leach-cifs-v1-spec-01
                     // http://msdn.microsoft.com/en-us/library/aa365247%28VS.85%29.aspx
-                    if (!preg_match('/^[^.]([^"\/\\\[\]:+|<>=;,*?. ]+){0,1}([.]'.
-                      '[^"\/\\\[\]:+|<>=;,*?. ]+){0,}$/', $value)) {
+                    if (!preg_match(
+                        '/^[^"\/\\\[\]:+|<>=;,*?. ]+(?:\.[^"\/\\\[\]:+|<>=;,*?. ]+)*$/',
+                        $value
+                    )) {
                         throw new \OMV\Json\SchemaValidationException(
                             "%s: The value '%s' is not a valid share name.",
                             $name,

deb/openmediavault/workbench/src/app/shared/forms/custom-validators.ts
@@ -47,7 +47,7 @@ const regExp = {
   // References:
   // http://tools.ietf.org/html/draft-leach-cifs-v1-spec-01
   // http://msdn.microsoft.com/en-us/library/aa365247%28VS.85%29.aspx
-  shareName: /^[^.]([^"/\\\[\]:+|<>=;,*?. ]+){0,1}([.][^"/\\\[\]:+|<>=;,*?. ]+){0,}$/,
+  shareName: /^[^"/\\\[\]:+|<>=;,*?. ]+(?:\.[^"/\\\[\]:+|<>=;,*?. ]+)*$/,
   ipv4NetCidr:
     /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\/(3[0-2]|[0-2]?[0-9])$/,
   ipv6NetCidr: /^(?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4}\/(12[0-8]|1[0-1][0-9]|[1-9][0-9]|[0-9])$/i,

deb/openmediavault/usr/share/openmediavault/unittests/php/Makefile
@@ -26,7 +26,7 @@ test:
 	@set -e; \
 	for file in $(PHP_FILES); do \
 		echo ">>> Processing $$file ..."; \
-		phpunit --include-path "$(PHP_INCLUDE_PATH)" --bootstrap "$(PHP_BOOTSTRAP_FILE)" $$file; \
+		phpunit --colors=auto --include-path "$(PHP_INCLUDE_PATH)" --bootstrap "$(PHP_BOOTSTRAP_FILE)" $$file; \
 		echo; \
 	done
 

deb/openmediavault/usr/share/openmediavault/unittests/php/test_openmediavault_datamodel_schema.php
@@ -507,6 +507,54 @@ class test_openmediavault_datamodel_schema extends \PHPUnit\Framework\TestCase
     /**
      * @doesNotPerformAssertions
      */
+    public function testCheckFormatSharename1()
+    {
+        $schema = $this->getSchema();
+        $schema->checkFormat("data", [ "format" => "sharename" ], "field1");
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCheckFormatSharename2()
+    {
+        $schema = $this->getSchema();
+        $schema->checkFormat("my.share", [ "format" => "sharename" ], "field1");
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCheckFormatSharename3()
+    {
+        $schema = $this->getSchema();
+        $schema->checkFormat("Hörbücher", [ "format" => "sharename" ], "field1");
+    }
+
+    public function testCheckFormatSharenameFail1()
+    {
+        $schema = $this->getSchema();
+        $this->expectException(\OMV\Json\SchemaValidationException::class);
+        $schema->checkFormat(".foo", [ "format" => "sharename" ], "field1");
+    }
+
+    public function testCheckFormatSharenameFail2()
+    {
+        $schema = $this->getSchema();
+        $this->expectException(\OMV\Json\SchemaValidationException::class);
+        $schema->checkFormat("bar..foo", [ "format" => "sharename" ], "xyz");
+    }
+
+    public function testCheckFormatSharenameFail3()
+    {
+        $schema = $this->getSchema();
+        $this->expectException(\OMV\Json\SchemaValidationException::class);
+        $schema->checkFormat("my share", [ "format" => "sharename" ], "field1");
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
     public function testCheckFormatNetbios1()
     {
         $schema = $this->getSchema();

deb/openmediavault/usr/share/openmediavault/unittests/python/test_openmediavault_datamodel_schema.py
@@ -284,6 +284,14 @@ class SchemaTestCase(unittest.TestCase):
         schema = openmediavault.datamodel.Schema({})
         schema._check_format("data", {"format": "sharename"}, "field1")
 
+    def test_check_format_sharename_2(self):
+        schema = openmediavault.datamodel.Schema({})
+        schema._check_format("my.share", {"format": "sharename"}, "field1")
+
+    def test_check_format_sharename_3(self):
+        schema = openmediavault.datamodel.Schema({})
+        schema._check_format("Hörbücher", {"format": "sharename"}, "field1")
+
     def test_check_format_sharename_fail_1(self):
         schema = openmediavault.datamodel.Schema({})
         self.assertRaises(
@@ -299,6 +307,15 @@ class SchemaTestCase(unittest.TestCase):
             openmediavault.json.SchemaValidationException,
             lambda: schema._check_format(
                 "bar..foo", {"format": "sharename"}, "xyz"
+            )
+        )
+
+    def test_check_format_sharename_fail_3(self):
+        schema = openmediavault.datamodel.Schema({})
+        self.assertRaises(
+            openmediavault.json.SchemaValidationException,
+            lambda: schema._check_format(
+                "my share", {"format": "sharename"}, "field1"
             )
         )
 

deb/openmediavault/workbench/src/app/shared/forms/custom-validators.spec.ts
@@ -450,6 +450,53 @@ describe('CustomValidators', () => {
       });
     });
 
+    describe('shareName', () => {
+      let validator: ValidatorFn;
+
+      beforeEach(() => {
+        validator = CustomValidators.patternType('shareName');
+      });
+
+      it('should validate shareName [1]', () => {
+        formControl.setValue('shared');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate shareName [2]', () => {
+        formControl.setValue('my.share');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should validate shareName [3]', () => {
+        formControl.setValue('Hörbücher');
+        expect(validator(formControl)).toBeNull();
+      });
+
+      it('should not validate shareName [1] (leading period)', () => {
+        formControl.setValue('.share');
+        expect(validator(formControl)).toEqual({
+          pattern:
+            'This field contains invalid characters, e.g. a blank or "/[]:+|<>=;,*? character.'
+        });
+      });
+
+      it('should not validate shareName [2] (consecutive periods)', () => {
+        formControl.setValue('my..share');
+        expect(validator(formControl)).toEqual({
+          pattern:
+            'This field contains invalid characters, e.g. a blank or "/[]:+|<>=;,*? character.'
+        });
+      });
+
+      it('should not validate shareName [3] (blank)', () => {
+        formControl.setValue('my share');
+        expect(validator(formControl)).toEqual({
+          pattern:
+            'This field contains invalid characters, e.g. a blank or "/[]:+|<>=;,*? character.'
+        });
+      });
+    });
+
     describe('userName', () => {
       let validator: ValidatorFn;
 